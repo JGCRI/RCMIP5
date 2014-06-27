bf9d312bbcc80ee66b972aa7bb4de916905a46d0
K Todd-Brown <ktoddbrown@gmail.com>
2014-06-27 15:53:47 -0500

    Removing all zero sized files is easy!

--- devMain.R
+++ devMain.R
@@ -1,3 +1,6 @@
 source('getFileInfo.R')
 
 fileInfo <- getFileInfo('/Volumes/DATAFILES/downloads')
+##example remove the empty files that failed to download
+#file.remove(as.character(fileInfo[fileInfo$fileSize == 0, 1]))
+
