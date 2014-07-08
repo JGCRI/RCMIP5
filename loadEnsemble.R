4c87fc35fcc6072e380d94a2a4e716e363ca0b27
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2014-07-08 09:00:12 -0400

    Tweaks and roxygen comments

--- loadEnsemble.R
+++ loadEnsemble.R
@@ -1,27 +1,35 @@
 library('raster')
 library('ncdf4')
 
-loadEnsemble <- function(CMIP5dir='.', experiment=NULL, variable=NULL,
-                          model=NULL, ensemble=NULL, recursive=TRUE){
+#' Load data for a particular experiment/variable/model/ensemble combination
+#'
+#' @param path root of directory tree
+#' @param experiment CMIP5 experiment to load
+#' @param variable CMIP5 variable to load
+#' @param model CMIP5 model to load
+#' @param ensemble CMIP5 ensemble to load
+#' @param recursive logical. Should we recurse into directories?
+#' @return RasterStack object with all loaded data
+#' @examples
+#' loadEnsemble(model="GFDL-CM3",variable="prc",experiment="rcp85",ensemble="r1i1p1")
+loadEnsemble <- function(path='.', experiment=NULL, variable=NULL,
+                          model=NULL, ensemble=NULL, recursive=TRUE) {
 
-    ##Pull a list of all files that match specifications
-    fileList <- list.files(path=CMIP5dir, pattern=sprintf('%s_[a-zA-Z]+_%s_%s_%s_', variable, model, experiment, ensemble), full.names=TRUE, recursive=recursive)
-
-    cat('loading:\n')
-    print(fileList)
-
+    # List all files that match specifications
+    # TODO this doesn't filter correctly if any of the above parameters is NULL
+    fileList <- list.files(path=path, pattern=sprintf('%s_[a-zA-Z]+_%s_%s_%s_', variable, model, experiment, ensemble), full.names=TRUE, recursive=recursive)
+    
     ensembleRst <- NULL
-
-    for(filename in fileList){
-        if(is.null(ensembleRst)){
-            ensembleRst <- brick(filename, varname=variable, lvar=3) #may also need to id lvar=3
-        }else{
+    for(f in fileList) {
+        cat('Loading ',f)
+        if(is.null(ensembleRst)) {
+            ensembleRst <- brick(f, varname=variable, lvar=3) # may also need to id lvar=3
+        } else {
             ##TODO This doesn't work with GFDL; they used non-uniform grids
             ##     Possible reason to switch to ncdf4 directly
-            ##TODO Append data more elegantly, we loose all the header info
-            ##     currently.
+            ##TODO Append data more elegantly, we lose all the header info
             ensembleRst <- addLayer(ensembleRst,
-                                    brick(filename, varname=variable, lvar=3))
+                                    brick(f, varname=variable, lvar=3))
         }
     }
 
