f76eaa18b0c3a33ae09e28c65b40c61eb01bd0f0
K Todd-Brown <ktoddbrown@gmail.com>
2014-07-09 19:30:46 -0500

    revise to take an array instead of a raster. This needs optimization!

--- makeAnnualMean.R
+++ makeAnnualMean.R
@@ -1,14 +1,31 @@
-library('raster')
-library('ncdf4')
-
-makeAnnualMean <- function(rasterObj){
+makeAnnualMean <- function(temp.ls){
     ##Input: Takes a raster object like the one returned by either loadEnsemble
     ##       and loadModel
-    yrIndex <- as.numeric(substr(names(rasterObj), 2, 5))
 
-    meanRst <- stackApply(rasterObj, yrIndex-yrIndex[1] + 1,
-                          fun=mean, na.rm=FALSE)
+    numDays <- as.numeric(substr(temp.ls$calendarStr, 1, 3))
 
-    #names(meanRst) <- unique(yrIndex)
-    return(meanRst)
+    startYr <- as.numeric(unlist(strsplit(regmatches(temp.ls$timeUnit,
+                          regexpr('\\d+.\\d+.\\d+', temp.ls$timeUnit)), '-')))
+    startYr <- startYr[1]+(startYr[2]-1)/12+(startYr[3]-1)/numDays
+
+    yrIndex <- temp.ls$time/numDays + startYr
+
+    ans <- vapply(unique(floor(yrIndex)), function(x){
+        aaply(temp.ls$val[,,x==floor(yrIndex)], c(1,2), mean)
+    }, FUN.VALUE=temp.ls$val[,,1])
+
+    #ans <- apply(temp.ls$val, c(1,2), function(x){
+    #     vapply(unique(floor(yrIndex)), function(y){
+    #         mean(x[y==floor(yrIndex)])
+    #}, FUN.VALUE=1.0)})
+
+    #ans <- a_ply(temp.ls$val, c(1,2), function(x){
+    #     vapply(unique(floor(yrIndex)), function(y){
+    #         mean(x[y==floor(yrIndex)])
+    #}, FUN.VALUE=1.0)})
+
+    return(list(val=ans, files=temp.ls$files,
+                year=unique(floor(yrIndex)),
+                lat=temp.ls$lat, lon=temp.ls$lon,
+                valUnit=temp.ls$valUnit))
 }
