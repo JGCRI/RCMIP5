f028e54e5c68b8c1bb07e11cd76aee3be541bd47
K Todd-Brown <ktoddbrown@gmail.com>
2014-07-14 16:48:58 -0500

    Time several different ways to calculate the annual mean, surprisingly the for loop is the fastest. Definitely opportunity here for more optimization in the future.

--- makeAnnualMean.R
+++ makeAnnualMean.R
@@ -1,7 +1,5 @@
+#library('plyr')
 makeAnnualMean <- function(temp.ls){
-    ##Input: Takes a raster object like the one returned by either loadEnsemble
-    ##       and loadModel
-
     numDays <- as.numeric(substr(temp.ls$calendarStr, 1, 3))
 
     startYr <- as.numeric(unlist(strsplit(regmatches(temp.ls$timeUnit,
@@ -10,22 +8,36 @@ makeAnnualMean <- function(temp.ls){
 
     yrIndex <- temp.ls$time/numDays + startYr
 
-    ans <- vapply(unique(floor(yrIndex)), function(x){
-        aaply(temp.ls$val[,,x==floor(yrIndex)], c(1,2), mean)
-    }, FUN.VALUE=temp.ls$val[,,1])
+    #ptm <- proc.time()
+    ##user  system elapsed 79.655   0.301  80.285 for apply
+    ##user  system elapsed 438.284   1.423 441.075 for aaply
+    ans <- array(NA, dim=c(dim(temp.ls$val)[c(1,2)], length(unique(floor(yrIndex)))))
+    counter <- 1
+    for(ii in unique(floor(yrIndex))){
+        #cat(ii, ' ')
+        ans[,,counter] <- aaply(temp.ls$val[,,ii == floor(yrIndex)], c(1,2), mean)
+        counter <- counter + 1
+    }
 
+    ##user  system elapsed  78.618   0.275  79.111
+    ##This function is a little faster but changes the order of the dimentions
+    ##...from [lon, lat, time] to [time, lon, lat], recasting would negate
+    ##...advantage probably
+    #ans <- vapply(unique(floor(yrIndex)), function(x){
+    #    cat(x, ' ')
+    #    return(apply(temp.ls$val[,,x==floor(yrIndex)], c(1,2), mean))
+    #}, FUN.VALUE=temp.ls$val[,,1])
+
+    ##user  system elapsed 202.290   7.138 210.332
     #ans <- apply(temp.ls$val, c(1,2), function(x){
     #     vapply(unique(floor(yrIndex)), function(y){
     #         mean(x[y==floor(yrIndex)])
     #}, FUN.VALUE=1.0)})
 
-    #ans <- a_ply(temp.ls$val, c(1,2), function(x){
-    #     vapply(unique(floor(yrIndex)), function(y){
-    #         mean(x[y==floor(yrIndex)])
-    #}, FUN.VALUE=1.0)})
-
+    #print(proc.time()-ptm)
     return(list(val=ans, files=temp.ls$files,
                 year=unique(floor(yrIndex)),
+                numMonths=table(floor(yrIndex)),
                 lat=temp.ls$lat, lon=temp.ls$lon,
                 valUnit=temp.ls$valUnit))
 }
