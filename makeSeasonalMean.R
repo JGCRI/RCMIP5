97c92c32216b2170cc9cdf7bb20ae820411dded2
K Todd-Brown <ktoddbrown@gmail.com>
2014-07-14 16:49:37 -0500

    Calculate the monthly values over several years.

--- makeSeasonalMean.R
+++ makeSeasonalMean.R
@@ -1,18 +1,31 @@
-library('raster')
-library('ncdf')
+#library('plyr')
+makeSeasonalMean <- function(temp.ls, yrRange){
 
-makeSeasonalMean <- function(rasterObj, yrRange){
+    numDays <- as.numeric(substr(temp.ls$calendarStr, 1, 3))
 
-    yrIndex <- as.numeric(substr(names(rasterObj), 2, 5))
-    yrSubset <- yrIndex >= yrRange$min & yrIndex <= yrRange$max
-    mntIndex <- as.numeric(substr(names(rasterObj), 7, 8))
+    startYr <- as.numeric(unlist(strsplit(regmatches(temp.ls$timeUnit,
+                          regexpr('\\d+.\\d+.\\d+', temp.ls$timeUnit)), '-')))
+    startYr <- startYr[1]+(startYr[2]-1)/12+(startYr[3]-1)/numDays
 
-    meanRst <- subset(rasterObj, subset=yrIndex[yrSubset])
+    yrIndex <- temp.ls$time/numDays + startYr
+    mntIndex <- floor((yrIndex%% 1)*12+1)
 
-    meanRst <- stackApply(meanRst, mntIndex[yrSubset],
-                          fun=mean, na.rm=FALSE)
 
-    names(meanRst) <- unique(mntIndex)
-    return(meanRst)
+    ans <- array(NA, dim=c(dim(temp.ls$val)[c(1,2)], 12))
+    counter <- 1
+    for(ii in 1:12){
+        #cat(ii, ': ', which((ii == mntIndex) & floor(yrIndex) >= yrRange$min & floor(yrIndex) <= yrRange$max), '\n')
+        ans[,,counter] <- aaply(temp.ls$val[,,(ii == mntIndex) &
+                                            floor(yrIndex) >= yrRange$min &
+                                            floor(yrIndex) <= yrRange$max],
+                                c(1,2), mean)
+        counter <- counter + 1
+    }
+
+
+    return(list(val=ans, files=temp.ls$files,
+                lat=temp.ls$lat, lon=temp.ls$lon,
+                valUnit=temp.ls$valUnit))
+
 
 }
