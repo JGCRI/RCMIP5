aba67d24ffe5957119f1e4f4b09a470cd5158505
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2014-07-23 00:32:11 -0400

    Testing code

--- tests/test_getFileInfo.R
+++ tests/test_getFileInfo.R
@@ -1,8 +1,9 @@
-# Testing code for the RCMIP5 (?) scripts
+# Testing code for the RCMIP5 (?) 'getFileInfo.R' script
 
 # Uses the testthat package
 # See http://journal.r-project.org/archive/2011-1/RJournal_2011-1_Wickham.pdf
 library(testthat)
+#source("getFileInfo.R")    # can't do this here
 
 # To run this code: 
 #   test_dir("tests/")
@@ -11,7 +12,7 @@ library(testthat)
 context("getFileInfo")
 
 test_that("getFileInfo handles bad input", {
-    expect_error(getFileInfo("dskjfjhkds"),"file.exists\\(path) is not TRUE")
+    expect_error(getFileInfo("does_not_exist"),"file.exists\\(path) is not TRUE")
                                                             # path does not exist
     expect_error(getFileInfo(path=c("1","2")))              # multi-value path
     expect_error(getFileInfo(recursive=c(T,F)))             # multi-value recursive
@@ -19,10 +20,25 @@ test_that("getFileInfo handles bad input", {
     expect_error(getFileInfo(recursive=1))                  # non-logical recursive
 })
 
-test_that("getFileInfo handles no input", {
+test_that("getFileInfo handles no input", {                 # no netcdf files found
     options(warn=-1)
-    expect_warning(getFileInfo(),"No netcdf files found")   # no netcdf files found
-    expect_is(getFileInfo(),'NULL')                         # no netcdf files found
+    expect_warning(getFileInfo("testdata_none/"),"No netcdf files found")   
+    expect_is(getFileInfo("testdata_none/"),'NULL')
     options(warn=0)
 })
 
+test_that("getFileInfo handles non-CMIP5 netcdfs", {        # improper netcdf filenames
+    w <- getOption('warn')
+    options(warn=-1)
+    expect_warning(getFileInfo("testdata_badfilename/"),"No netcdf files found")   
+    expect_is(getFileInfo("testdata_badfilename/"),'NULL')
+    options(warn=w)
+})
+
+test_that("getFileInfo handles annual netcdfs", { 
+    # TODO
+})
+
+test_that("getFileInfo handles monthly netcdfs", { 
+    # TODO
+})
