641739a81118592eb135d08f29e4e84c87b2ebe9
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2014-08-02 14:46:08 -0400

    Removed 'prcTemp' ref and renamed to computeYearIndex

--- R/internalHelpers.R
+++ R/internalHelpers.R
@@ -1,6 +1,7 @@
 #' Compute year index from parsed cmip5data information
 #'
 #' @param x cmip5data A structure returned from loadEnsemble() or loadModel()
+#' @param debug boolean Print debug messages?
 #' @return yearIndex A numeric vector of years
 #' @details This function uses information in a \code{\link{cmip5data}} object
 #' to compute the yearIndex, i.e. the (perhaps fractional) years associated with
@@ -8,11 +9,11 @@
 #' number of days per year; parsing \code{timeUnit} to get the starting date;
 #' and then using the values in the \code{time} vector.
 #' @note This is an internal RCMIP5 function and not exported.
-compute_yearIndex <- function(x) {
+computeYearIndex <- function(x, debug=FALSE) {
     stopifnot(class(x)=="cmip5data")
 
     # Pull a numeric from the calendarStr and assume it's the days
-    numDays <- as.numeric(regmatches(prcTemp$calendarStr, regexpr('\\d+', prcTemp$calendarStr)))
+    numDays <- as.numeric(regmatches(x$calendarStr, regexpr('\\d+', x$calendarStr)))
     # If nothing matches set it to a default of 365
     if(length(numDays) == 0) numDays <- 365
     stopifnot(is.numeric(numDays) & numDays>0)
@@ -28,4 +29,4 @@ compute_yearIndex <- function(x) {
     stopifnot(startYrArr[3] %in% 1:31)
 
     return(x$time/numDays + startYr)
-} # compute_yearIndex
+} # computeYearIndex
