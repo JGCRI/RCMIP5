591e205fc0eb8fb985ea6010b55354144110931a
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2014-08-09 14:58:59 -0400

    Duh - put things back in correct order

--- sourceall.R
+++ sourceall.R
@@ -4,13 +4,16 @@ library(testthat)
 load("data/tas_Amon_CMCC-CESM_historical_r1i1p1.Rdata")
 load("data/tas_Amon_CMCC-CESM_rcp85_r1i1p1.Rdata")
 
-source("R/checkTimePeriod.R")
-source("R/devMain.R")
 source("R/getFileInfo.R")
-source("R/internalHelpers.R")
+source("R/checkTimePeriod.R")
+
 source("R/loadEnsemble.R")
 source("R/loadModel.R")
+
+source("R/RCMIP5.R")
+source("R/internalHelpers.R")
 source("R/makeAnnualStat.R")
 source("R/makeGlobalStat.R")
 source("R/makeMonthlyStat.R")
-source("R/RCMIP5.R")
+
+source("R/devMain.R")
