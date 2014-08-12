8be7f998942a0e9fa3183c3c0f1b11fcdad1c367
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2014-08-11 22:15:09 -0400

    makeGlobalStat now returns correct dimension order

--- R/makeGlobalStat.R
+++ R/makeGlobalStat.R
@@ -9,11 +9,16 @@ library(abind)
 #' @param parallel logical. Parallelize if possible?
 #' @param FUN function. Function to apply across grid
 #' @param ... Other arguments passed on to \code{FUN}
-#' @return A \code{\link{cmip5data}} object.
+#' @return A \code{\link{cmip5data}} object, in which the \code{val} dimensions are the
+#' same as the caller for lev/depth (if present) and time, but lon and lat are reduced to 
+#' 1 (i.e. no dimensionality). A \code{numCells} field is also added, recording the number
+#' of cells in the spatial grid.
+#' @details If 'lev' and/or 'depth' dimensions are present, the stat function is calculated
+#' for all combinations of these. 
 #' @note We expect that weighted.mean and a weighted sum will be the most frequent
 #' calculations needed. The former is built into R, and the latter can generally
-#' be calculated as weighted.mean * sum(area).
-#' A user-supplied function must follow the weighted.mean syntax, in particular 
+#' be calculated as weighted.mean * sum(area). A user-supplied stat function must 
+#' follow the weighted.mean syntax, in particular 
 #' accepting parameters 'x' (data) and 'w' (weights) of equal size.
 #' @export
 makeGlobalStat <- function(x, area=NULL, verbose=TRUE, parallel=FALSE, FUN=weighted.mean, ...) {
@@ -31,7 +36,7 @@ makeGlobalStat <- function(x, area=NULL, verbose=TRUE, parallel=FALSE, FUN=weigh
     # Get and check area data, using 1's if nothing supplied
     areavals <- array(1, dim=dim(x$val)[1:2])
     if(is.null(area)) {
-        if(verbose) cat("Using unweighted areas\n")  # TODO: should this be warning?
+        if(verbose) warning("No grid areas supplied; global stat will probably not be correct\n")
     } else {
         stopifnot(identical(x$lat, area$lat) & identical(x$lon, area$lon))  # must match
         areavals <- area$val
@@ -45,14 +50,38 @@ makeGlobalStat <- function(x, area=NULL, verbose=TRUE, parallel=FALSE, FUN=weigh
     
     # Main computation code
     if(parallel) parallel <- require(foreach) & require(doParallel) & require(abind)
-    if(verbose) cat("Running in", ifelse(parallel, "parallel", "serial"), "\n")
-    timer <- system.time( # time the main computation
-        x$val <- unname(aaply(x$val, c(3:timeIndex), .fun=FUN, w=areavals, .parallel=parallel, ...))
-    ) # system.time
+    timer <- system.time({ # time the main computation
+        margins <- NULL
+        if(timeIndex > 3) margins <- c(3:(timeIndex-1))
+        ans <- list()
+        
+        if(parallel) {  # go parallel, woo hoo!
+            registerDoParallel()
+            if(verbose) cat("Running in parallel [", getDoParWorkers(), "cores ]\n")
+            ans <- foreach(i=1:length(x$time), .combine = function(...) abind(..., along=timeIndex-2), .packages='plyr') %dopar% {
+                aaply(asub(x$val, idx=x$time[i] == x$time, dims=timeIndex), 
+                      margins, FUN, w=areavals, ...)                
+            }
+        } else {
+            if(verbose) cat("Running in serial\n")
+            for(i in 1:length(x$time)) {
+                if(verbose & floor(i/10)==i/10) cat(i, " ")
+                ans[[i]] <- aaply(asub(x$val, idx=x$time[i] == x$time, dims=timeIndex), 
+                                  margins, FUN, w=areavals, ...)
+            }
+            # All done, now combine answer list with correct 'along' ordering
+            # (When both lev and depth are present, timeIndex=5, along=3. When only
+            # one is, TimeIndex=4, along=2; and when no lev/depth info, 3/1.)
+            # The abind'ing is done by foreach in the parallel logic above.
+            ans <- abind(ans, along=timeIndex-2)
+        }
+        dim(ans) <- c(1, 1, dim(ans))      # add back in spatial dimensions of 1 (as placeholders)      
+    }) # system.time
     
     if(verbose) cat('\nTook', timer[3], 's\n')
     
     # Finish up
+    x$val <- ans
     x[c('lat', 'lon')] <- NULL
     x$variable <- paste(as.character(substitute(FUN)), "of", x$variable)
     x$numCells <- length(areavals)
