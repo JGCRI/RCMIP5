e83d04b93353fa8ae37ba1a9cc4562ac5e6aee7a
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2014-08-11 23:06:44 -0400

    thinking about filtering

--- R/filterDimensions.R
+++ R/filterDimensions.R
@@ -1,20 +1,31 @@
 #' Filter dimensions, limiting to arbitrary lon/lat/time/lev/depth
 #'
 #' @param x cmip5data A \code{\link{cmip5data}} object.
+#' @param lons numeric vector. Longitudes to filter (in).
+#' @param lats numeric vector. Latitudes to filter (in).
+#' @param depths numeric vector. Depths to filter.
+#' @param levs numeric vector
 #' @param years numeric vector
 #' @param months numeric vector
-#' @param levs numeric vector
-#' @param depths numeric vector. Depths to filter.
-#' @param mask numeric array. Spatial mask of same dimensions as object.
 #' @param verbose logical. Print info as we go?
-#' @param parallel logical. Parallelize if possible?
 #' @return A \code{\link{cmip5data}} object.
 #' @export
 #' @examples
-filterDimensions <- function() {
+filterDimensions <- function(x, lons=NULL, lats=NULL, depths=NULL, levs=NULL, 
+                             years=NULL, months=NULL, verbose=FALSE) {
     
     # TODO: Do we want a function that does this? It's going to be very common
     # for users to only care about surface CO2, for example, and they don't want
     # makeAnnualMean() to run computations on all levels.
+
+    # Sanity checks
+    stopifnot(class(x)=="cmip5data")
+    stopifnot(is.null(lons) | class(lons)=="numeric")
+    stopifnot(is.null(lats) | class(lats)=="numeric")
+    stopifnot(is.null(depths) | class(depths)=="numeric")
+    stopifnot(is.null(levs) | class(levs)=="numeric")
+    stopifnot(is.null(years) | class(years)=="numeric")
+    stopifnot(is.null(months) | class(months)=="numeric")
+    stopifnot(length(verbose)==1 & is.logical(verbose))
     
 } # filterDimensions
