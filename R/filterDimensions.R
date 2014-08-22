ec77b710bef0cebf4d904ecf826e57ee545d6aa3
K Todd-Brown <ktoddbrown@gmail.com>
2014-08-22 15:01:28 -0500

    code up years filter

--- R/filterDimensions.R
+++ R/filterDimensions.R
@@ -10,23 +10,10 @@
 #' @param verbose logical. Print info as we go?
 #' @return A \code{\link{cmip5data}} object.
 #' @export
-filterDimensions <- function(x, lons=NULL, lats=NULL, depths=NULL, levs=NULL, 
+filterDimensions <- function(x, lons=NULL, lats=NULL, depths=NULL, levs=NULL,
                              years=NULL, months=NULL, verbose=FALSE) {
-    
-    # TODO: Do we want a function that does this? It's going to be very common
-    # for users to only care about surface CO2, for example, and they don't want
-    # makeAnnualMean() to run computations on all levels.
 
-    # TODO: ask 'mask' (spatial lon/lat mask) above?
-    
-    # TODO: Should variables above be bounds, e.g. to filter to western hemisphere
-    #   lons=c(-180, 0)
-    # or straight numbers
-    #   lons=c(-180, -175, ..., 0)
-    # ? The first is simpler, while the second requires more work from the user but
-    # gives more flexibility. I'm inclined to go with simple.
-    
-    # Sanity checks
+    ## Sanity checks
     stopifnot(class(x)=="cmip5data")
     stopifnot(is.null(lons) | class(lons)=="numeric")
     stopifnot(is.null(lats) | class(lats)=="numeric")
@@ -35,5 +22,43 @@ filterDimensions <- function(x, lons=NULL, lats=NULL, depths=NULL, levs=NULL,
     stopifnot(is.null(years) | class(years)=="numeric")
     stopifnot(is.null(months) | class(months)=="numeric")
     stopifnot(length(verbose)==1 & is.logical(verbose))
-    
+
+    # KTB do we need to copy this?
+    ans <- x
+    # Check to see if there is a constraint on the years we want to pull
+    if(!is.null(years)){
+        if(length(dim(temp$val)) == 3){
+            ans$val <- ans$val[,,floor(ans$time) %in% years]
+        }else if(length(dim(temp$val)) == 4){
+            ans$val <- ans$val[,,,floor(ans$time) %in% years]
+        }else{
+            # do nothing since, ordering of ans$val dimentions is
+            # ...[lon, lat, ?depth/lev, time] so anything other then 3 or 4 is
+            # ...not valid
+            warning("Years specified for time invarient value.")
+            break #skip out on the rest of the if statement
+        }
+        ans$time <- ans$time[floor(ans$time) %in% years]
+        ans$provenance <- addProvenance(ans$provenance,
+                                        paste("Filtering for years in range [",
+                                              paste(range(years),
+                                                    collapse=', '), "]"))
+    }
+
+    # TODO: Do we want a function that does this? It's going to be very common
+    # for users to only care about surface CO2, for example, and they don't want
+    # makeAnnualMean() to run computations on all levels.
+
+    # TODO: ask 'mask' (spatial lon/lat mask) above?
+
+    # TODO: Should variables above be bounds, e.g. to filter to western hemisphere
+    #   lons=c(-180, 0)
+    # or straight numbers
+    #   lons=c(-180, -175, ..., 0)
+    # ? The first is simpler, while the second requires more work from the user but
+    # gives more flexibility. I'm inclined to go with simple.
+
+
+
+    return(ans)
 } # filterDimensions
