7b5a7a4366c92a1b966b7ff70980bec378265683
K Todd-Brown <ktoddbrown@gmail.com>
2014-08-25 12:40:51 -0500

    Now loads and averages soil carbon correctly.

--- examples/plotModernNEE.R
+++ examples/plotModernNEE.R
@@ -1,17 +1,28 @@
-source('../R/RCMIP5.R')
-source('../sourceall.R')
+setwd(gsub('/examples$', '', getwd()))
+source('sourceall.R')
 
+cat('starting plotModernNEE.R\n')
 expStr <- 'historical'
-yearArr <- 1995:2004
+yearArr <- as.numeric(1995:2004)
 
-modelArr <- 'bcc-csm1-1'
+modelArr <- c('bcc-csm1-1-m')
+
+CMIP5Path <- '/Volumes/DATAFILES/downloads/'
 
 for(modelStr in modelArr){
-    soil <- loadModel(experiment=expStr, model=modelStr, variable='cSoil')
-    cwd <-  loadModel(experiment=expStr, model=modelStr, variable='cCwd')
-    litter <-  loadModel(experiment=expStr, model=modelStr, variable='cLitter')
-    veg <-  loadModel(experiment=expStr, model=modelStr, variable='cVeg')
-    gpp <-  loadModel(experiment=expStr, model=modelStr, variable='gpp')
+    gridArea <- loadModel(experiment=expStr, model=modelStr, variable='areacella', path=CMIP5Path)
+    landPer <- loadModel(experiment=expStr, model=modelStr, variable='sftlf', path=CMIP5Path)
+    soil <- loadModel(experiment=expStr, model=modelStr, variable='cSoil', path=CMIP5Path)
+    soil10yr <- filterDimensions(soil, years=yearArr)
+    soilModern <- list(val=aaply(soil10yr$val, 1:2, mean),
+                       area=gridArea$val*landPer$val/100,
+                       lat=soil10yr$lat, lon=soil10yr$lon,
+                       valUnit=soil10yr$valUnit)
+    soilRegrid <- regrid_data(soilModern, 1)
+    #cwd <-  loadModel(experiment=expStr, model=modelStr, variable='cCwd')
+    #litter <-  loadModel(experiment=expStr, model=modelStr, variable='cLitter')
+    #veg <-  loadModel(experiment=expStr, model=modelStr, variable='cVeg')
+    #gpp <-  loadModel(experiment=expStr, model=modelStr, variable='gpp')
 
 
 }
