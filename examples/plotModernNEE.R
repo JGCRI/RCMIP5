5eb05a41dedf1a35b7ceeea10534e3d9c558efd5
K Todd-Brown <ktoddbrown@gmail.com>
2014-08-25 08:01:17 -0500

    Sketching out the function

--- examples/plotModernNEE.R
+++ examples/plotModernNEE.R
@@ -1,5 +1,17 @@
 source('../R/RCMIP5.R')
+source('../sourceall.R')
 
 expStr <- 'historical'
 yearArr <- 1995:2004
 
+modelArr <- 'bcc-csm1-1'
+
+for(modelStr in modelArr){
+    soil <- loadModel(experiment=expStr, model=modelStr, variable='cSoil')
+    cwd <-  loadModel(experiment=expStr, model=modelStr, variable='cCwd')
+    litter <-  loadModel(experiment=expStr, model=modelStr, variable='cLitter')
+    veg <-  loadModel(experiment=expStr, model=modelStr, variable='cVeg')
+    gpp <-  loadModel(experiment=expStr, model=modelStr, variable='gpp')
+
+
+}
