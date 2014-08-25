c044d2a5aea1d9035f407b871ac29318adbeac05
K Todd-Brown <ktoddbrown@gmail.com>
2014-08-25 14:01:09 -0500

    Continue dev. Checking in so that I can update the other files.

--- examples/plotModernNEE.R
+++ examples/plotModernNEE.R
@@ -3,26 +3,51 @@ source('sourceall.R')
 
 cat('starting plotModernNEE.R\n')
 expStr <- 'historical'
-yearArr <- as.numeric(1995:2004)
+yearArr <- as.numeric(1995:2005)
 
 modelArr <- c('bcc-csm1-1-m')
 
 CMIP5Path <- '/Volumes/DATAFILES/downloads/'
 
+landC.ls <- list()
 for(modelStr in modelArr){
-    gridArea <- loadModel(experiment=expStr, model=modelStr, variable='areacella', path=CMIP5Path)
+    cat('loading variables for [', modelStr, ']\n')
+    cat('loading grid area...')
+    gridArea <- loadModel(experiment=expStr, model=modelStr, variable='areacella', path=CMIP5Path, verbose=TRUE)
+    print(summary(gridArea))
+    cat('\nloading land percentage...')
     landPer <- loadModel(experiment=expStr, model=modelStr, variable='sftlf', path=CMIP5Path)
-    soil <- loadModel(experiment=expStr, model=modelStr, variable='cSoil', path=CMIP5Path)
-    soil10yr <- filterDimensions(soil, years=yearArr)
-    soilModern <- list(val=aaply(soil10yr$val, 1:2, mean),
-                       area=gridArea$val*landPer$val/100,
-                       lat=soil10yr$lat, lon=soil10yr$lon,
-                       valUnit=soil10yr$valUnit)
-    soilRegrid <- regrid_data(soilModern, 1)
-    #cwd <-  loadModel(experiment=expStr, model=modelStr, variable='cCwd')
-    #litter <-  loadModel(experiment=expStr, model=modelStr, variable='cLitter')
-    #veg <-  loadModel(experiment=expStr, model=modelStr, variable='cVeg')
-    #gpp <-  loadModel(experiment=expStr, model=modelStr, variable='gpp')
+
+    print(summary(landPer))
+
+    #Load soil
+    landC <- NULL
+    cat('loading land carbon\n')
+    for(varStr in c('cSoil', 'cCwd', 'cLitter', 'cVeg')){
+        cat('loading [', varStr, ']...')
+        temp <- loadModel(experiment=expStr, model=modelStr,
+                          variable=varStr, path=CMIP5Path)
+        if(is.null(temp)){
+            cat('no', varStr, 'found... moving on\n')
+        }else{
+            cat('pulling dC...')
+            temp10yr <- makeAnnualStat(filterDimensions(temp, years=yearArr), FUN=mean)
+            tempdC <- temp10yr$val[,,2:11] - temp10yr$val[,,1:10]
+            tempModern <- list(val=aaply(tempdC, 1:2, mean),
+                               area=gridArea$val*landPer$val/100,
+                               lat=temp10yr$lat, lon=temp10yr$lon,
+                               valUnit=paste(temp10yr$valUnit, 'yr^-1'))
+            tempRegrid <- regrid_data(tempModern, 1)
+            if(is.null(landC)){
+                landC <- tempRegrid
+            }else{
+                landC$val <- landC$val + tempRegrid$val
+            }
+        }
+    }
+    cat('done and saving\n')
+
+    landC.ls[[modelStr]] <- landC
 
 
 }
