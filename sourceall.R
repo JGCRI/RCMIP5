7835121745f1ba2a9a12164943e9860d5dc69ac5
K Todd-Brown <ktoddbrown@gmail.com>
2014-08-25 12:39:38 -0500

    Start area weighted regridding
    Initialize the area weighted regridding functions with the old stuff
    from KTB. Generally works but likely needs a lot of optimization and
    does not deal with depth/levels.

--- sourceall.R
+++ sourceall.R
@@ -25,4 +25,10 @@ source("R/makeMonthlyStat.R")
 source("R/mergeExperiments.R")
 source("R/filterDimensions.R")
 
-source("R/devMain.R")
+#KTB old area weighted regridding functions
+#...these are bloated and have lots of room
+#...for optimization but do provide a starting point.
+source("R/calcGridArea.R")
+source("R/regrid_val.R")
+source("R/regrid_data.R")
+#source("R/devMain.R")
