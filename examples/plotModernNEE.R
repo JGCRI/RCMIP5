e9d63f413058f572ddf07fafbc4e7b731f954b19
K Todd-Brown <ktoddbrown@gmail.com>
2014-08-25 19:12:01 -0500

    Should now work

--- examples/plotModernNEE.R
+++ examples/plotModernNEE.R
@@ -5,20 +5,34 @@ cat('starting plotModernNEE.R\n')
 expStr <- 'historical'
 yearArr <- as.numeric(1995:2005)
 
-modelArr <- c('bcc-csm1-1-m')
+modelArr <- c('bcc-csm1-1-m', 'BNU-ESM', 'CanESM2', 'CESM1-BGC',  'HadGEM2-ES',
+              'inmcm4', 'IPSL-CM5A-LR', 'MIROC-ESM', 'MPI-ESM-LR')
 
 CMIP5Path <- '/Volumes/DATAFILES/downloads/'
-
-landC.ls <- list()
-for(modelStr in modelArr){
+load('examples/plotModernNEE.RData')
+if(! 'landC.ls' %in% ls()){
+    landC.ls <- list()
+}
+for(modelStr in setdiff(modelArr, names(landC.ls))){
     cat('loading variables for [', modelStr, ']\n')
     cat('loading grid area...')
     gridArea <- loadModel(experiment=expStr, model=modelStr, variable='areacella', path=CMIP5Path, verbose=TRUE)
+    #dim(gridArea$val) <- dim(gridArea$val)[1:2]
     print(summary(gridArea))
     cat('\nloading land percentage...')
     landPer <- loadModel(experiment=expStr, model=modelStr, variable='sftlf', path=CMIP5Path)
-
+    #dim(landPer$val) <- dim(landPer$val)[1:2]
     print(summary(landPer))
+
+    if(is.null(landPer)){
+        cat('Setting land percentage to one\n')
+        landPer$val <- 100
+    }
+
+    if(max(as.vector(landPer$val), na.rm=TRUE) != 100){
+        cat('bad land percentage... moving on\n')
+        next
+    }
 
     #Load soil
     landC <- NULL
@@ -27,11 +41,16 @@ for(modelStr in modelArr){
         cat('loading [', varStr, ']...')
         temp <- loadModel(experiment=expStr, model=modelStr,
                           variable=varStr, path=CMIP5Path)
+        if(is.null(gridArea)){
+            gridArea <- list(val=calcGridArea(lat=temp$lat,
+                                              lon=temp$lon))
+        }
         if(is.null(temp)){
             cat('no', varStr, 'found... moving on\n')
         }else{
             cat('pulling dC...')
-            temp10yr <- makeAnnualStat(filterDimensions(temp, years=yearArr), FUN=mean)
+            temp10yr <- makeAnnualStat(filterDimensions(temp, years=yearArr),
+                                       FUN=mean)
             tempdC <- temp10yr$val[,,2:11] - temp10yr$val[,,1:10]
             tempModern <- list(val=aaply(tempdC, 1:2, mean),
                                area=gridArea$val*landPer$val/100,
@@ -43,11 +62,36 @@ for(modelStr in modelArr){
             }else{
                 landC$val <- landC$val + tempRegrid$val
             }
+            cat(sprintf('%.2f Pg-C yr^-1\n',
+                        sum(as.vector(tempModern$val *
+                                      gridArea$val*landPer$val/100),
+                            na.rm=TRUE)/1e12))
+
         }
     }
     cat('done and saving\n')
 
     landC.ls[[modelStr]] <- landC
-
+    save(file='examples/plotModernNEE.RData', landC.ls)
 
 }
+
+cat('finding min/max...')
+minGrid <- landC.ls[[1]]$val
+maxGrid <- landC.ls[[1]]$val
+for(modelStr in names(landC.ls)){
+    minGrid <- pmin(minGrid, landC.ls[[modelStr]]$val)
+    maxGrid <- pmax(minGrid, landC.ls[[modelStr]]$val)
+}
+
+world.plot(lon=landC.ls[[1]]$lon, lat=landC.ls[[1]]$lat, x=minGrid,
+           main='minimum [kg m^-2 yr^-1]', centerZero=TRUE, absNum=c(-0.5, 0.5))
+dev.new()
+world.plot(lon=landC.ls[[1]]$lon, lat=landC.ls[[1]]$lat, x=maxGrid,
+           main='maximum [kg m^-2 yr^-1]', centerZero=TRUE, absNum=c(-0.5, 0.5))
+dev.new()
+world.plot(lon=landC.ls[[1]]$lon, lat=landC.ls[[1]]$lat, x=maxGrid-minGrid,
+           main='range [kg m^-2 yr^-1]', absNum=c(0, 0.1))
+
+globalMin <- sum(minGrid*landC.ls[[1]]$area, na.rm=TRUE)/1e12
+globalMax <- sum(maxGrid*landC.ls[[1]]$area, na.rm=TRUE)/1e12
