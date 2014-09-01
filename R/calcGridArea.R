4d80c0ac9c717c405215bfb70e91376e20fcc144
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2014-09-01 07:49:23 -0400

    Cleaned up, commented

--- R/calcGridArea.R
+++ R/calcGridArea.R
@@ -1,49 +1,59 @@
-##Programer: Kathe Todd-Brown ktoddbrown@gmail.com
-##Date: commented 23 April 2013
-##Purpose: Calculate the grid cella area for a centered lat/lon grid
-##Input: Accepts the latitude and longitude defining a global grid
-##       Currently the lon must be uniform but the lat does not need to be
-##Output: Returns the grid cell area in m^2 (meter*meter)
-
-calcGridArea<- function(lat, lon){
-#lat, lon coordinates for the centers of our grids of interest
-  cat('calculating area...')
-  numLat <- length(lat)
-  numLon <- length(lon)
-
-  ##Dummy check the given lat/lon
-  if(abs(lat[1]) == 90){
-      cat('Error: lat centered at 90\n')
-      return(NA)
-  }
-  if(lon[1] == 0){
-    if(lon[numLon] == 360){
-      cat('Error: lon centered at 0 and 360')
-      return(NA)
+#' Calculate the grid cella area for a centered lat/lon grid.
+#' 
+#' Calculate the grid cella area for a centered lat/lon grid.
+#'
+#' @param lat latitude coordinates of the grid centers
+#' @param lon longitude coordinates of the grid centers
+#' @param verbose logical. Print info as we go?
+#' @return The grid cell area in m^2 (meter*meter).
+#' @details Currently the lon must be uniform but the lat does not need to be.
+#' @note This is an internal RCMIP5 function and not exported.
+calcGridArea<- function(lon, lat, verbose=FALSE) {
+    
+    # Sanity checks - parameter classes and lengths
+    stopifnot(is.numeric(lat))
+    stopifnot(is.numeric(lon))
+    stopifnot(length(verbose)==1 & is.logical(verbose))
+    
+    if(verbose) cat('Calculating area...\n')
+    numLat <- length(lat)
+    numLon <- length(lon)
+    
+    # Dummy check the given lat/lon
+    # TODO: why is this happening?
+    if(abs(lat[1]) != 90) {
+        stop('Error: lat centered at 90\n')
     }
-    #shift the longitude over
-    lon <- c(lon[2:numLon], 360)
-  }
-
-  ##Hackish, assume that the edges are at 0 and 360
-  temp <- (lon[2:numLon]-lon[1:(numLon-1)])/2
-  deltaLon <- c(lon[1] + temp[1],
-                temp[2:length(temp)] + temp[2:length(temp)-1],
-                360-rev(lon)[1]+rev(temp)[1])
-
-  ##Assume the radius of the earth
-  N <- 6371*1e3 ##Don't get too fancy, agrees better with area calc from models then trying to back out the NS/WE plane radius
-  N <- matrix(N, nrow=numLon, ncol=numLat, byrow=TRUE)
-
-  ##Find the edges of the latitude grid
-  midLat <- (lat[2:numLat]+lat[1:(numLat-1)])/2
-  latMin <- c(-90, midLat)
-  latMax <- c(midLat, 90)
-
-  latMin <- matrix(latMin, nrow=numLon, ncol=numLat, byrow=TRUE)
-  latMax <- matrix(latMax, nrow=numLon, ncol=numLat, byrow=TRUE)
-  deltaLon <- matrix(deltaLon, nrow=numLon, ncol=numLat)
-
-  cat('done\n')
-  return(N^2*(sin(latMax/180*pi)-sin(latMin/180*pi))*deltaLon/180*pi)
-}
+    if(lon[1] == 0) {
+        if(lon[numLon] == 360) {
+            stop('Error: lon centered at 0 and 360')
+        }
+        lon <- c(lon[2:numLon], 360)        # shift the longitude over
+    }
+    
+    # Hackish, assume that the edges are at 0 and 360. TODO: change?
+    temp <- (lon[2:numLon]-lon[1:(numLon-1)])/2
+    deltaLon <- c(lon[1] + temp[1],
+                  temp[2:length(temp)] + temp[2:length(temp)-1],
+                  360-rev(lon)[1]+rev(temp)[1])
+    
+    # Assume the radius of the earth. Don't get too fancy: 6371e3 assumption agrees
+    # better with area calc from models then trying to back out the NS/WE plane radius
+    radius <- 6371e3 # meters
+    N <- matrix(radius, nrow=numLon, ncol=numLat, byrow=TRUE)
+    
+    # Find the edges of the latitude grid
+    # BBL: these calculations need to be better documented TODO
+    midLat <- (lat[2:numLat]+lat[1:(numLat-1)])/2
+    if(verbose) cat("midLat =", midLat, "\n")
+    latMin <- c(-90, midLat)
+    latMax <- c(midLat, 90)
+    
+    latMin <- matrix(latMin, nrow=numLon, ncol=numLat, byrow=TRUE)
+    latMax <- matrix(latMax, nrow=numLon, ncol=numLat, byrow=TRUE)
+    deltaLon <- matrix(deltaLon, nrow=numLon, ncol=numLat)
+    
+    N <- N^2 * (sin(latMax/180*pi) - sin(latMin/180*pi)) * deltaLon/180 * pi
+    if(verbose) cat('Done\n')
+    invisible(N)
+} # calcGridArea
