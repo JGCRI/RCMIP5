a489d2efee67cf34c2d2337d9d21b433c6672daf
K Todd-Brown <ktoddbrown@gmail.com>
2014-09-29 10:22:09 -0500

    Commenting and algorithm update. No significant change to the outputs.

--- R/calcGridArea.R
+++ R/calcGridArea.R
@@ -20,38 +20,44 @@ calcGridArea<- function(lon, lat, verbose=FALSE) {
     numLat <- length(lat)
     numLon <- length(lon)
 
-    # Dummy check the given lat/lon - BBL: why? Clarify
-    if(abs(lat[1]) == 90) {
-        stop('Error: lat centered at 90\n')
-    }
-    if(lon[1] == 0) {
-        if(lon[numLon] == 360) {
-            stop('Error: lon centered at 0 and 360')
-        }
-        lon <- c(lon[2:numLon], 360)        # shift the longitude over
+    # Check that the latitudes are centered in the grids
+    if(any(abs(lat) == 90)) {
+        stop('No grid cell can be centered at pole.')
     }
 
-    # Hackish, assume that the edges are at 0 and 360. TODO: change?
-    temp <- (lon[2:numLon]-lon[1:(numLon-1)])/2
-    deltaLon <- c(lon[1] + temp[1],
-                  temp[2:length(temp)] + temp[2:length(temp)-1],
-                  360-rev(lon)[1]+rev(temp)[1])
+    # If for some reason we have a -180:180 lon base, reset to span 0:360
+    if(any(lon < 0)){
+        lon <- lon + 180
+    }
 
-    # Assume the radius of the earth. Don't get too fancy: 6371e3 assumption agrees
-    # better with area calc from models then trying to back out the NS/WE plane radius
-    radius <- 6371e3 # meters
-    N <- matrix(radius, nrow=numLon, ncol=numLat, byrow=TRUE)
+    # Calculate the longitude degrees spanned by a grid cell
+    # ... modulo 360 to deal with wrapping boundries
+    deltaLon <- (lon[c(2:length(lon),1)] - lon[1:length(lon)]) %% 360
 
-    # Find the edges of the latitude grid
-    # BBL: these calculations need to be better documented
-    midLat <- (lat[2:numLat]+lat[1:(numLat-1)])/2
-#    if(verbose) cat("midLat =", midLat, "\n")
-    latMin <- c(-90, midLat)
-    latMax <- c(midLat, 90)
+    # Calculate the min/max latitude for each grid cell
+    edgeLat <- (lat[2:length(lat)]+lat[2:length(lat)-1])/2
+    minLat <- c(-90, edgeLat)
+    maxLat <- c(edgeLat, 90)
 
-    latMin <- matrix(latMin, nrow=numLon, ncol=numLat, byrow=TRUE)
-    latMax <- matrix(latMax, nrow=numLon, ncol=numLat, byrow=TRUE)
-    deltaLon <- matrix(deltaLon, nrow=numLon, ncol=numLat)
+    # Convert from degree to radius
+    deltaLon <- matrix(deltaLon/180*pi,  nrow=length(lon), ncol=length(lat))
+    minLat <- matrix(minLat/180*pi,  nrow=length(lon), ncol=length(lat), byrow=TRUE)
+    maxLat <- matrix(maxLat/180*pi,  nrow=length(lon), ncol=length(lat), byrow=TRUE)
+    lat <- matrix(lat/180*pi,   nrow=length(lon), ncol=length(lat), byrow=TRUE)
 
-    invisible(N^2 * (sin(latMax/180*pi) - sin(latMin/180*pi)) * deltaLon/180 * pi)
+    # Assume the radius of the earth: 6371e3 meter
+    R <- 6371e3 # meters
+
+    # Calculate the east/west edges by assuming the earth is spherical and
+    # ...east/west edges are defined by latitude arc lengths
+    # ... => R*(latMax-latMin)
+    # Calculate the north/south edges by assuming the arc length of longitude
+    # ...is the lattitude corrected radius (R*cos(lat)) times the change in lon
+    # ... => (R*cos(lat))*deltaLon
+    return( R*(latMax-latMin) * (R*cos(lat))*deltaLon)
+
+    #old formulation for reference (updated 29 September 2014)
+    # ...no significant difference but harder to explain
+    #R^2*(sin(maxLat)-sin(minLat))*deltaLon
+
 } # calcGridArea
