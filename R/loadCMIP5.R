2bd0c53ddc1b73c592920d607a01068fb7799edc
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2014-10-04 09:31:44 -0400

    loadCMIP5 can compute min/max/sum/mean
    Addresses issue #82.

--- R/loadCMIP5.R
+++ R/loadCMIP5.R
@@ -1,8 +1,8 @@
 #' Load CMIP5 data
 #'
 #' Loads CMIP5 data from disk. \code{loadCMIP5} will return a unique model ensemble,
-#' or will average all ensemble members of a specified experiment-variable-model
-#' combination.
+#' or will apply a function across all ensemble members of a 
+#' specified experiment-variable-model combination.
 #'
 #' @param variable CMIP5 variable to load (required)
 #' @param model CMIP5 model to load (required)
@@ -13,11 +13,15 @@
 #' @param recursive logical. Should we recurse into directories?
 #' @param verbose logical. Print info as we go?
 #' @param force.ncdf Force use of the less-desirable ncdf package for testing?
+#' @param FUN function. Function (mean, min, max, or sum) to apply across ensembles
 #' @param yearRange numeric of length 2. If supplied, load only these years of data
 #' @return A \code{\link{cmip5data}} object
 #' @note The \code{yearRange} parameter is intended to help users deal with large
 #' CMIP5 data files on memory-limited machines, e.g. by allowing them to process
 #' smaller chunks of such files.
+#' @note FUN is limited to min, max, sum, and mean (the default), because the 
+#' memory costs of keeping all ensembles in memory is too high. Be warned that 
+#' min and max are quite slow!
 #' @examples
 #' \dontrun{
 #' loadCMIP5(experiment='rcp85', variable='prc', model='GFDL-CM3', ensemble='r1i1p1')
@@ -25,8 +29,8 @@
 #' @export
 loadCMIP5 <- function(variable, model, experiment, ensemble=NULL, domain='[^_]+',
                       path='.', recursive=TRUE, verbose=FALSE, force.ncdf=FALSE,
-                      yearRange=NULL) {
-
+                      FUN=mean, yearRange=NULL) {
+    
     # Sanity checks - parameters are correct type and length
     stopifnot(length(variable)==1 & is.character(variable))
     stopifnot(length(model)==1 & is.character(model))
@@ -39,7 +43,9 @@ loadCMIP5 <- function(variable, model, experiment, ensemble=NULL, domain='[^_]+'
     stopifnot(length(verbose)==1 & is.logical(verbose))
     stopifnot(length(force.ncdf)==1 & is.logical(force.ncdf))
     stopifnot(is.null(yearRange) | length(yearRange)==2 & is.numeric(yearRange))
-
+    FUNstr <- as.character(substitute(FUN))
+    stopifnot(FUNstr %in% c("mean", "min", "max", "sum"))
+    
     # If a unique ensemble is specified, jump right to loadEnsemble()
     if(!is.null(variable) & !is.null(model)
        &!is.null(experiment) & !is.null(ensemble)) {
@@ -47,38 +53,38 @@ loadCMIP5 <- function(variable, model, experiment, ensemble=NULL, domain='[^_]+'
                             path=path, recursive=recursive, verbose=verbose,
                             force.ncdf=force.ncdf, yearRange=yearRange))
     }
-
+    
     # List all files that match specifications
     fileList <- list.files(path=path, full.names=TRUE, recursive=recursive)
-
+    
     # Only pull the files which are specified by the id strings
     fileList <- fileList[grepl(pattern=sprintf('^%s_%s_%s_%s_',
-                                          variable, domain, model, experiment),
+                                               variable, domain, model, experiment),
                                basename(fileList))]
-
+    
     if(length(fileList) == 0) {
         warning("Could not find any matching files")
         return(NULL)
     }
-
+    
     # Strip the .nc out of the file list
     fileList <- gsub('\\.nc$', '', fileList)
-
+    
     # Parse out the ensemble strings according to CMIP5 specifications for
     # ...file naming conventions
     ensembleArr <- unique(unlist(lapply(strsplit(basename(fileList), '_'),
                                         function(x){x[5]})))
-
+    
     if(verbose) cat('Averaging ensembles:', ensembleArr, '\n')
-
+    
     modelTemp <- NULL              # Initalize the return data structure
     for(ensemble in ensembleArr) { # for each ensemble...
-
+        
         # load the entire ensemble
         temp <- loadEnsemble(variable, model, experiment, ensemble, domain,
                              path=path, verbose=verbose, recursive=recursive,
                              force.ncdf=force.ncdf, yearRange=yearRange)
-
+        
         if(is.null(modelTemp)) {         # If first model, just copy
             modelTemp <- temp
         } else {
@@ -88,29 +94,42 @@ loadCMIP5 <- function(variable, model, experiment, ensemble=NULL, domain='[^_]+'
                    identical(temp$lon, modelTemp$lon) &
                    identical(temp$Z, modelTemp$Z) &
                    identical(temp$time, modelTemp$time)) {
+                
                 # Add this ensemble's data and record file and ensemble loaded
-                modelTemp$val <- modelTemp$val + temp$val
+                if(FUNstr %in% c("min", "max")) { # for min and max, compute as we go
+                    if(verbose) cat("Computing", FUNstr)
+                    combined <- array(c(modelTemp$val, temp$val), dim=c(dim(modelTemp$val), 2))
+                    modelTemp$val <- aaply(combined, 
+                                           .margins=1:length(dim(modelTemp$val)),
+                                           .fun=FUN,
+                                           .progress=ifelse(verbose, "text", "none"))
+                } else { # mean and sum are easier, and much faster
+                    modelTemp$val <- modelTemp$val + temp$val
+                }
+                
                 modelTemp$files <- c( modelTemp$files, temp$files )
                 modelTemp$ensembles <- c(modelTemp$ensembles, ensemble)
                 modelTemp <- addProvenance(modelTemp, temp)
                 modelTemp <- addProvenance(modelTemp, paste("Added ensemble", ensemble))
-            } else { # ...if dimensions don't match, don't load and warn user
+            } else { # ...if dimensions don't match, don't load
                 warning(ensemble,
                         'Not loaded: data dimensions do not match those of previous ensemble(s)\n')
             }
         } # is.null(modelTemp)
     } # for
-
+    
     # Make sure at least one ensemble was actually loaded
     if(length(modelTemp$ensembles) == 0) {
         warning("No ensembles were loaded.")
         return(NULL)
     }
-
-    # Compute mean over all ensembles, update provenance, and return
-    modelTemp$val <- unname(modelTemp$val / length(modelTemp$ensembles))
-    modelTemp <- addProvenance(modelTemp, c(paste("Computed mean of ensembles:",
+    
+    # If taking the mean, calculate over all ensembles
+    if(FUNstr == "mean")
+        modelTemp$val <- unname(modelTemp$val / length(modelTemp$ensembles))
+    
+    # Update provenance and return
+    addProvenance(modelTemp, c(paste("Computed", FUNstr, "of ensembles:",
                                                   paste(ensembleArr, collapse=' '))))
-    return(modelTemp)
 } # loadCMIP5
 
