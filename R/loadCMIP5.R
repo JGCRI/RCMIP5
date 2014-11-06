JGCRI/RCMIP5

commit b33438f4b5ac5baec3721f1a0a8f7be1bf2633c8

loadCMIP5 now honors ensemble parameter

R/loadCMIP5.R
@@ -27,7 +27,7 @@
 #' loadCMIP5(experiment='rcp85', variable='prc', model='GFDL-CM3', ensemble='r1i1p1')
 #' }
 #' @export
-loadCMIP5 <- function(variable, model, experiment, ensemble=NULL, domain='[^_]+',
+loadCMIP5 <- function(variable, model, experiment, ensemble='[^_]+', domain='[^_]+',
                       path='.', recursive=TRUE, verbose=FALSE, force.ncdf=FALSE,
                       FUN=mean, yearRange=NULL) {
     
@@ -50,8 +50,8 @@ loadCMIP5 <- function(variable, model, experiment, ensemble=NULL, domain='[^_]+'
     fileList <- list.files(path=path, full.names=TRUE, recursive=recursive)
     
     # Only pull the files which are specified by the id strings
-    fileList <- fileList[grepl(pattern=sprintf('^%s_%s_%s_%s_.*\\.nc$',
-                                               variable, domain, model, experiment),
+    fileList <- fileList[grepl(pattern=sprintf('^%s_%s_%s_%s_%s_.*\\.nc$',
+                                               variable, domain, model, experiment, ensemble),
                                basename(fileList))]
     
     #cat(fileList)
@@ -86,9 +86,9 @@ loadCMIP5 <- function(variable, model, experiment, ensemble=NULL, domain='[^_]+'
         } else {
             # Make sure lat-lon-Z-time match
             if(all(identical(temp$lat, modelTemp$lat) &
-                   identical(temp$lon, modelTemp$lon) &
-                   identical(temp$Z, modelTemp$Z) &
-                   identical(temp$time, modelTemp$time))) {
+                       identical(temp$lon, modelTemp$lon) &
+                       identical(temp$Z, modelTemp$Z) &
+                       identical(temp$time, modelTemp$time))) {
                 
                 # Add this ensemble's data and record file and ensemble loaded
                 if(FUNstr %in% c("min", "max")) { # for min and max, compute as we go

commit 978536e4250a4fdc801187d51417b499b2bce22a

Fixes problems introduced in commit b33438f4b5
…causing test failures, and other problems.

R/loadCMIP5.R
@@ -50,7 +50,7 @@ loadCMIP5 <- function(variable, model, experiment, ensemble='[^_]+', domain='[^_
     fileList <- list.files(path=path, full.names=TRUE, recursive=recursive)
     
     # Only pull the files which are specified by the id strings
-    fileList <- fileList[grepl(pattern=sprintf('^%s_%s_%s_%s_%s_.*\\.nc$',
+    fileList <- fileList[grepl(pattern=sprintf('^%s_%s_%s_%s_%s.*\\.nc$',
                                                variable, domain, model, experiment, ensemble),
                                basename(fileList))]
     

tests/testthat/test_loadCMIP5.R
@@ -93,7 +93,6 @@ test_that("loadCMIP5 can load using both ncdf and ncdf4", {
 })
 
 test_that("loadCMIP5 can load area files", {
-    skip_on_cran()
 
     path <- "../../sampledata/fx/"
     if(!file.exists(path)) skip("Path doesn't exist")

tests/testthat/test_loadEnsemble.R
@@ -107,8 +107,10 @@ test_that("loadEnsemble handles 2D lon and lat", {
     
     d <- loadEnsemble('tos','GFDL-ESM2G', 'historical', 'r1i1p1', '[^_]+', path=path, verbose=F)
     expect_is(d, "cmip5data")
-    expect_equal(length(dim(d$lon)), 1)
-    expect_equal(length(dim(d$lat)), 1)
+    expect_is(d$lon, "numeric")
+    expect_is(d$lat, "numeric")
+    expect_null(dim(d$lon))
+    expect_null(dim(d$lat))    
 })
 
 test_that("loadEnsemble handles data with time length=1", {