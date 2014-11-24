39d02f9750fa4f7db24ef452b1a29ad65f4ae66b
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2014-11-24 10:42:46 -0500

    Removed support for 'ncdf' from saveNetCDF()

--- R/saveNetCDF.R
+++ R/saveNetCDF.R
@@ -10,15 +10,13 @@
 #' @param verbose logical. Print info as we go?
 #' @param saveProvenance Save the provenance separately?
 #' @param originalNames logical. Use original dimension names from file?
-#' @param force.ncdf Force use of the older ncdf package for testing?
 #' @return The fully-qualified filename that was written (invisible).
 #' @details If no filename is provided, a meaningful one will be assigned based on the
 #' CMIP5 naming convention (but appending 'RCMIP5'). \code{\link{loadCMIP5}} should be
 #' able to read this file. If \code{saveProvenance} is specified, the provenance is saved
 #' separately in a comma-separated file of the same name but appending "_prov.csv".
 #' (Provenance messages are always saved as NetCDF file attributes.)
-saveNetCDF <- function(x, file=NULL, path="./", verbose=FALSE, 
-                       saveProvenance=TRUE, originalNames=FALSE, force.ncdf=FALSE) {
+saveNetCDF <- function(x, file=NULL, path="./", verbose=FALSE, saveProvenance=TRUE, originalNames=FALSE) {
     
     # Sanity checks - class and length of parameters
     stopifnot(class(x)=="cmip5data")
@@ -32,28 +30,9 @@ saveNetCDF <- function(x, file=NULL, path="./", verbose=FALSE,
         dimNames <- c("lon", "lat", "Z", "time")
     if(verbose) cat("Writing with names", dimNames, "\n")
     
-    # We prefer to use the 'ncdf4' package, but Windows has problems with this, so
-    # if it's not installed can also use 'ncdf'
-    if(force.ncdf | !require(ncdf4, quietly=!verbose)) {
-        if(require(ncdf, quietly=!verbose)) {
-            # The ncdf and ncdf4 functions are mostly parameter-identical. This makes
-            # things easy--we redefine the ncdf4 function names to their ncdf equivalents
-            .nc_create <- ncdf::create.ncdf
-            .ncdim_def <- ncdf::dim.def.ncdf
-            .ncvar_def <- ncdf::var.def.ncdf
-            .ncatt_put <- ncdf::att.put.ncdf
-            .ncvar_put <- ncdf::put.var.ncdf
-            .nc_close <- ncdf::close.ncdf
-        } else {
-            stop("No NetCDF (either 'ncdf4' or 'ncdf') package is available")            
-        }
-    } else {
-        .nc_create <- ncdf4::nc_create
-        .ncdim_def <- ncdf4::ncdim_def
-        .ncvar_def <- ncdf4::ncvar_def
-        .ncatt_put <- ncdf4::ncatt_put
-        .ncvar_put <- ncdf4::ncvar_put
-        .nc_close <- ncdf4::nc_close
+    # loadEnsemble() handles both ncdf and ncdf4, but saveNetCDF only supports the latter
+    if(!require(ncdf4, quietly=!verbose)) {
+        stop("This requires the 'ncdf4' package. Download it from CRAN or http://cirrus.ucsd.edu/~pierce/ncdf/")            
     }
     
     # Create meaningful filename, if necessary
@@ -71,18 +50,18 @@ saveNetCDF <- function(x, file=NULL, path="./", verbose=FALSE,
     if(verbose) cat("Defining NetCDF dimensions...")
     dimlist <- list()
     if(!is.null(x$lon) & !is.null(x$lat)) {
-        londim <- .ncdim_def("lon", x$debug$lonUnit, x$lon)
-        latdim <- .ncdim_def("lat", x$debug$latUnit, x$lat)
+        londim <- ncdf4::ncdim_def("lon", x$debug$lonUnit, x$lon)
+        latdim <- ncdf4::ncdim_def("lat", x$debug$latUnit, x$lat)
         dimlist <- list(londim, latdim) # for now assume no Z/time        
     }
     
     # Define Z and time dimensions, if present
     if(!is.null(x$Z)) {
-        Zdim <- .ncdim_def(dimNames[3], x$debug$ZUnit, x$Z)
+        Zdim <- ncdf4::ncdim_def(dimNames[3], x$debug$ZUnit, x$Z)
         dimlist <- list(londim, latdim, Zdim)
     }
     if(!is.null(x$time)) {
-        timedim <- .ncdim_def(dimNames[length(dimNames)], x$debug$timeUnit, x$debug$timeRaw, calendar=x$debug$calendarStr)
+        timedim <- ncdf4::ncdim_def(dimNames[length(dimNames)], x$debug$timeUnit, x$debug$timeRaw, calendar=x$debug$calendarStr)
         dimlist[[length(dimlist)+1]] <- timedim     
     }
     
@@ -90,33 +69,33 @@ saveNetCDF <- function(x, file=NULL, path="./", verbose=FALSE,
     
     # Define mandatory variable
     if(verbose) cat("Defining main NetCDF variable\n")
-    valvar <- .ncvar_def(x$variable, x$valUnit, dimlist)
+    valvar <- ncdf4::ncvar_def(x$variable, x$valUnit, dimlist)
     
     # Create the file and write mandatory variable
     # Note we make sure data is sorted correctly first
     if(verbose) cat("Creating and writing", file, "\n")
-    nc <- .nc_create(fqfn, valvar)    
-    .ncvar_put(nc, valvar, as.array(x))
+    nc <- ncdf4::nc_create(fqfn, valvar)    
+    ncdf4::ncvar_put(nc, valvar, as.array(x))
     
     # Write spatial dimensions, if present
     if(!is.null(x$lon) & !is.null(x$lat)) {
         if(verbose) cat("Writing lon and lat\n")
-        lonvar <- .ncvar_def("lon", x$debug$lonUnit, londim)
-        latvar <- .ncvar_def("lat", x$debug$latUnit, londim)
-        .ncvar_put(nc, lonvar, x$lon)
-        .ncvar_put(nc, latvar, x$lat)        
+        lonvar <- ncdf4::ncvar_def("lon", x$debug$lonUnit, londim)
+        latvar <- ncdf4::ncvar_def("lat", x$debug$latUnit, londim)
+        ncdf4::ncvar_put(nc, lonvar, x$lon)
+        ncdf4::ncvar_put(nc, latvar, x$lat)        
     }
     
     # Write Z and time dimensions, if present
     if(!is.null(x$Z)) {
         if(verbose) cat("Writing Z\n")
-        Zvar <- .ncvar_def(dimNames[3], x$debug$ZUnit, Zdim)
-        .ncvar_put(nc, Zvar, x$Z) 
+        Zvar <- ncdf4::ncvar_def(dimNames[3], x$debug$ZUnit, Zdim)
+        ncdf4::ncvar_put(nc, Zvar, x$Z) 
     }
     if(!is.null(x$time)) {
         if(verbose) cat("Writing time\n")
-        timevar <- .ncvar_def(dimNames[4], x$debug$timeUnit, timedim)
-        .ncvar_put(nc, timevar, x$time) 
+        timevar <- ncdf4::ncvar_def(dimNames[4], x$debug$timeUnit, timedim)
+        ncdf4::ncvar_put(nc, timevar, x$time) 
     }
     
     # Get package version number, allowing that there might not be one
@@ -125,17 +104,17 @@ saveNetCDF <- function(x, file=NULL, path="./", verbose=FALSE,
     
     # Write attributes
     if(verbose) cat("Writing attributes\n")    
-    .ncatt_put(nc, 0, "software", paste("Written by RCMIP5", pkgv, 
+    ncdf4::ncatt_put(nc, 0, "software", paste("Written by RCMIP5", pkgv, 
                                         "under", R.version.string, date()))
     if(!is.null(x$time)) {
-        .ncatt_put(nc, 0, "frequency", x$debug$timeFreqStr)
+        ncdf4::ncatt_put(nc, 0, "frequency", x$debug$timeFreqStr)
     }
     for(i in 1:nrow(x$provenance)) {
-        .ncatt_put(nc, 0, paste0("provenance", i), x$provenance[i, "message"])
+        ncdf4::ncatt_put(nc, 0, paste0("provenance", i), x$provenance[i, "message"])
     }
     
-    # All done with NetCDF Close file, inform user
-    .nc_close(nc)
+    # All done. Close file, update provenance, return filename
+    ncdf4::nc_close(nc)
     if(verbose) cat("Wrote", round(file.info(fqfn)$size/1024/1024, 2), "MB\n")
     
     if(saveProvenance) {
