7b87d28b7f8e70ffaa7160c9134e3c084024ceb8
ktoddbrown <ktoddbrown@gmail.com>
2015-04-30 13:52:51 -0700

    set R directory path so that we can call from any directory

--- sourceall.R
+++ sourceall.R
@@ -1,3 +1,7 @@
+if(!'RDirPath' %in% ls()){
+    RDirPath <- 'R'
+}
+
 # File to source if not using package-development mode
 
 library(testthat)
@@ -14,28 +18,28 @@ library(assertthat)
 #load("data/tas_Amon_CMCC-CESM_historical_r1i1p1.Rdata")
 #load("data/tas_Amon_CMCC-CESM_rcp85_r1i1p1.Rdata")
 
-source("R/getFileInfo.R")
-source("R/checkTimePeriod.R")
+source(sprintf("%s/getFileInfo.R", RDirPath))
+source(sprintf("%s/checkTimePeriod.R", RDirPath))
 
-source("R/loadCMIP5.R")
-source("R/loadEnsemble.R")
+source(sprintf("%s/loadCMIP5.R", RDirPath))
+source(sprintf("%s/loadEnsemble.R", RDirPath))
 
-source("R/RCMIP5.R")
-source("R/addProvenance.R")
-source("R/makeAnnualStat.R")
-source("R/makeZStat.R")
-source("R/makeGlobalStat.R")
-source("R/makeMonthlyStat.R")
-source("R/mergeExperiments.R")
-source("R/filterDimensions.R")
-source("R/saveNetCDF.R")
+source(sprintf("%s/RCMIP5.R", RDirPath))
+source(sprintf("%s/addProvenance.R", RDirPath))
+source(sprintf("%s/makeAnnualStat.R", RDirPath))
+source(sprintf("%s/makeZStat.R", RDirPath))
+source(sprintf("%s/makeGlobalStat.R", RDirPath))
+source(sprintf("%s/makeMonthlyStat.R", RDirPath))
+source(sprintf("%s/mergeExperiments.R", RDirPath))
+source(sprintf("%s/filterDimensions.R", RDirPath))
+source(sprintf("%s/saveNetCDF.R", RDirPath))
 
 #KTB old area weighted regridding functions
 #...these are bloated and have lots of room
 #...for optimization but do provide a starting point.
-source("R/calcGridArea.R")
-#source("R/regridVal.R")
-#source("R/regridData.R")
+source(sprintf("%s/calcGridArea.R", RDirPath))
+#source(sprintf("%s/regridVal.R", RDirPath))
+#source(sprintf("%s/regridData.R", RDirPath))
 
-#source('R/world.plot.R')
-source("R/worldPlot.R")
+#source('%s/world.plot.R')
+source(sprintf("%s/worldPlot.R", RDirPath))
