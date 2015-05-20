0ee362dbe1b1330a8c6b72d2f8982f4e2b6223c5
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2015-05-20 14:10:40 -0400

    Implementation-aware tests

--- tests/testthat/test_makeGlobalStat.R
+++ tests/testthat/test_makeGlobalStat.R
@@ -12,6 +12,8 @@ library(testthat)
 
 context("makeGlobalStat")
 
+implementations <- c("data.frame", "array")
+
 test_that("makeGlobalStat handles bad input", {
     expect_error(makeGlobalStat(1))                         # non-list d
     expect_error(makeGlobalStat(cmpi5data()))               # wrong size list d
@@ -23,100 +25,117 @@ test_that("makeGlobalStat handles bad input", {
 
 test_that("makeGlobalStat handles monthly data", {
     years <- 1850:1851
-    d <- cmip5data(years)
-    res <- makeGlobalStat(d, verbose=F)
     
-    # Is 'res' correct type and size?
-    expect_is(res,"cmip5data")
-    
-    # Did unchanging info get copied correctly?
-    expect_equal(res$valUnit, d$valUnit)
-    expect_equal(res$files, d$files)
-    
-    # Lon/lat removed, numCells set, and provenance updated?
-    expect_null(res$lon)
-    expect_null(res$lat)
-    expect_is(res$numCells, "integer")
-    expect_more_than(nrow(res$provenance), nrow(d$provenance))
-    
-    # Does time match what we expect?
-    expect_equal(res$time, d$time)
-    
-    # Is the answer value data frame correctly sized?
-    expect_equal(nrow(res$val), nrow(d$val)/length(d$lon))
-    
-    # Are the answer values numerically correct?
-    expect_equal(mean(res$val$value), mean(d$val$value))  # no weighting
+    for(i in implementations) {
+        d <- cmip5data(years, loadAs=i)
+        res <- makeGlobalStat(d, verbose=F)
+        
+        # Is 'res' correct type and size?
+        expect_is(res,"cmip5data", info=i)
+        
+        # Did unchanging info get copied correctly?
+        expect_equal(res$valUnit, d$valUnit, info=i)
+        expect_equal(res$files, d$files, info=i)
+        
+        # Lon/lat removed, numCells set, and provenance updated?
+        expect_null(res$lon, info=i)
+        expect_null(res$lat, info=i)
+        expect_is(res$numCells, "integer", info=i)
+        expect_more_than(nrow(res$provenance), nrow(d$provenance), info=i)
+        
+        # Does time match what we expect?
+        expect_equal(res$time, d$time, info=i)
+        
+        # Is the answer value data frame correctly sized?
+        expect_equal(RCMIP5:::nvals(res), RCMIP5:::nvals(d)/length(d$lon), info=i)
+        
+        # Are the answer values numerically correct?
+        expect_equal(mean(RCMIP5:::vals(res)), mean(RCMIP5:::vals(d)), info=i)  # no weighting
+    }
 })
 
 test_that("makeGlobalStat weights correctly", {
-    d <- cmip5data(1850, randomize=T, monthly=F)
-    darea <- cmip5data(0, time=F, randomize=T)  # create an area file
-    
-    res <- makeGlobalStat(d, area=darea, sortData=F, verbose=F)
-    
-    # Are the answer values numerically correct?
-    dummyans <- weighted.mean(d$val$value, w=darea$val$value)
-    expect_equal(dummyans, res$val$value)
+    for(i in implementations) {
+        d <- cmip5data(1850, randomize=T, monthly=F, loadAs=i)
+        darea <- cmip5data(0, time=F, randomize=T, loadAs=i)  # create an area file
+        
+        res <- makeGlobalStat(d, area=darea, sortData=F, verbose=F)
+        
+        # Are the answer values numerically correct?
+        dummyans <- weighted.mean(RCMIP5:::vals(d), w=RCMIP5:::vals(darea))
+        expect_equal(dummyans, res$val$value, info=i)
+    }
 })
 
 test_that("weighted.sum works correctly", {
-#    d <- cmip5data(1850, randomize=T, monthly=F)
-#    darea <- cmip5data(0, time=F, randomize=T)
-#    res <- makeGlobalStat(d, area=darea, verbose=F, sortData=F, FUN=weighted.sum)
+    #    d <- cmip5data(1850, randomize=T, monthly=F)
+    #    darea <- cmip5data(0, time=F, randomize=T)
+    #    res <- makeGlobalStat(d, area=darea, verbose=F, sortData=F, FUN=weighted.sum)
     
     # Are the answer values numerically correct?
-#    dummyans <- weighted.sum(d$val$value, w=darea$val$value)
-#    expect_equal(dummyans, res$val$value)
+    #    dummyans <- weighted.sum(d$val$value, w=darea$val$value)
+    #    expect_equal(dummyans, res$val$value)
     
     # Make sure the function itself is OK
-#    expect_equal(weighted.sum(1:4), 10)
-#    expect_equal(weighted.sum(1:4, 1:4), 30) # 4*4 + 3*3 + 2*2 + 1*1
+    #    expect_equal(weighted.sum(1:4), 10)
+    #    expect_equal(weighted.sum(1:4, 1:4), 30) # 4*4 + 3*3 + 2*2 + 1*1
 })
 
 test_that("makeGlobalStat handles 4-dimensional data", {
-    years <- 1850:1851
-    d <- cmip5data(years, Z=T)
-    res <- makeGlobalStat(d, verbose=F)
-    
-    # Do years match what we expect?
-    expect_equal(res$time, d$time)
-    
-    # Is the answer value array correctly sized?
-    expect_equal(nrow(res$val), nrow(d$val)/length(d$lon))
+    for(i in implementations) {
+        years <- 1850:1851
+        d <- cmip5data(years, Z=T, loadAs=i)
+        res <- makeGlobalStat(d, verbose=F)
+        
+        # Do years match what we expect?
+        expect_equal(res$time, d$time, info=i)
+        
+        # Is the answer value array correctly sized?
+        expect_equal(RCMIP5:::nvals(res), RCMIP5:::nvals(d)/length(d$lon), info=i)
+    }
 })
 
 test_that("makeGlobalStat handles custom function and dots", {
     years <- 1850:1851
     llsize <- 2
-    d <- cmip5data(years, lonsize=llsize, latsize=llsize)
-    darea <- cmip5data(0, time=F, lonsize=llsize, latsize=llsize)
     
-    # All data 1 except for max lon/lat is 2
-    d$val$value <- 1
-    d$val$value[d$val$lon == max(d$lon) & d$val$lat == max(d$lat)] <- 2    
-    darea$val$value <- c(rep(1, llsize*llsize-1), llsize*llsize-1)
-    
-    # Compute correct answer
-    ans <- aggregate(value~time, data=d$val, FUN=weighted.mean, w=darea$val$value)
+    for(i in implementations) {
+        d <- cmip5data(years, lonsize=llsize, latsize=llsize, loadAs=i)
+        darea <- cmip5data(0, time=F, lonsize=llsize, latsize=llsize, loadAs=i)
         
-    res1 <- makeGlobalStat(d, darea, verbose=F, sortData=F, FUN=weighted.mean)
-    expect_is(res1, "cmip5data")
-    
-    myfunc <- function(x, w, ...) weighted.mean(x, w, ...)
-    res2 <- makeGlobalStat(d, darea, verbose=F, sortData=F, FUN=myfunc)
-    expect_is(res1, "cmip5data")
-    
-    # Are the result values correct?    
-    expect_equal(res1$val$value, ans$value)    
-    expect_equal(res2$val$value, ans$value)
+        # All data 1 except for max lon/lat is 2        
+        if(is.data.frame(d$val)) {
+            d$val$value <- 1
+            d$val$value[d$val$lon == max(d$lon) & d$val$lat == max(d$lat)] <- 2    
+            darea$val$value <- c(rep(1, llsize*llsize-1), llsize*llsize-1)
+        } else if(is.array(d$val)) {
+            d$val <- array(1, dim=dim(d$val))
+            d$val[ncol(d$lon),nrow(d$lat),,] <- 2
+            darea$val <- array(1, dim=dim(darea$val))
+        }
+        
+        # Compute correct answer
+        ans <- aggregate(value~time, data=d$val, FUN=weighted.mean, w=RCMIP5:::vals(darea))
+        
+        res1 <- makeGlobalStat(d, darea, verbose=F, sortData=F, FUN=weighted.mean)
+        expect_is(res1, "cmip5data", info=i)
+        
+        myfunc <- function(x, w, ...) weighted.mean(x, w, ...)
+        res2 <- makeGlobalStat(d, darea, verbose=F, sortData=F, FUN=myfunc)
+        expect_is(res1, "cmip5data", info=i)
+        
+        # Are the result values correct?    
+        expect_equal(RCMIP5:::vals(res1), RCMIP5:::vals(ans), info=i)    
+        expect_equal(RCMIP5:::vals(res2), RCMIP5:::vals(ans), info=i)
+    }
 })
 
 test_that("makeGlobalStat sorts before computing", {
     years <- 1850:1851
     llsize <- 2
-    d <- cmip5data(years, lonsize=llsize, latsize=llsize, monthly=F)
-    darea <- cmip5data(0, time=F, lonsize=llsize, latsize=llsize)
+    
+    d <- cmip5data(years, lonsize=llsize, latsize=llsize, monthly=F, loadAs="data.frame")
+    darea <- cmip5data(0, time=F, lonsize=llsize, latsize=llsize, loadAs="data.frame")
     
     # All data 1 except for max lon/lat is 2
     d$val$value <- 1
@@ -130,7 +149,7 @@ test_that("makeGlobalStat sorts before computing", {
     darea$val <- dplyr::arrange(darea$val, desc(lon), desc(lat))
     res1 <- makeGlobalStat(d, darea, verbose=F, sortData=TRUE)
     expect_is(res1, "cmip5data")
-
+    
     # makeGlobalStat should be sorted darea correctly before calculating
     # Are the result values correct?    
     expect_equal(res1$val$value, ans$value)
--- tests/testthat/test_makeZStat.R
+++ tests/testthat/test_makeZStat.R
@@ -12,6 +12,8 @@ library(testthat)
 
 context("makeZStat")
 
+implementations <- c("data.frame") #, "array")
+
 test_that("makeZStat handles bad input", {
     expect_error(makeZStat(1))                         # non-list d
     expect_error(makeZStat(cmpi5data()))               # wrong size list d
@@ -23,52 +25,63 @@ test_that("makeZStat handles bad input", {
 
 test_that("makeZStat computes Z means", {
     years <- 1850:1851
-    d <- cmip5data(years, Z=T)
-    res <- makeZStat(d, verbose=F)
-
-    # Is 'res' correct type and size?
-    expect_is(res, "cmip5data")
-
-    # Did unchanging info get copied correctly?
-    expect_equal(res$valUnit, d$valUnit)
-    expect_equal(res$files, d$files)
-    expect_equal(res$lon, d$lon)
-    expect_equal(res$lat, d$lat)
-    expect_equal(res$time, d$time)
     
-    # Depth removed, and other info updated?
-    expect_null(res$Z)
-    expect_is(res$numZs, "integer")
-    expect_more_than(nrow(res$provenance), nrow(d$provenance))
-
-    # Is the answer value array correctly sized?
-    expect_equal(nrow(res$val), nrow(d$val)/length(d$Z))
-    
-    # Are the answer values numerically correct?
-    expect_equal(mean(res$val$value), mean(d$val$value))
+    for(i in implementations) {
+        d <- cmip5data(years, Z=T, loadAs=i)
+        res <- makeZStat(d, verbose=F)
+        
+        # Is 'res' correct type and size?
+        expect_is(res, "cmip5data", info=i)
+        
+        # Did unchanging info get copied correctly?
+        expect_equal(res$valUnit, d$valUnit, info=i)
+        expect_equal(res$files, d$files, info=i)
+        expect_equal(res$lon, d$lon, info=i)
+        expect_equal(res$lat, d$lat, info=i)
+        expect_equal(res$time, d$time, info=i)
+        
+        # Depth removed, and other info updated?
+        expect_null(res$Z, info=i)
+        expect_is(res$numZs, "integer", info=i)
+        expect_more_than(nrow(res$provenance), nrow(d$provenance), info=i)
+        
+        # Is the answer value array correctly sized?
+        expect_equal(RCMIP5:::nvals(res), RCMIP5:::nvals(d)/length(d$Z), info=i)
+        
+        # Are the answer values numerically correct?
+        expect_equal(mean(RCMIP5:::vals(res)), mean(RCMIP5:::vals(d)), info=i)
+    }
 })
 
 test_that("makeZStat handles custom function and dots", {
     years <- 1850:1851
     llsize <- 2
-    d <- cmip5data(years, lonsize=llsize, latsize=llsize, Z=T)
     
-    # All data 1, except max Z is 2
-    d$val$value <- 1
-    d$val$value[d$val$Z ==max(d$Z)] <- 2    
-    w <- c(rep(1, length(d$Z)-1), length(d$Z)-1)
-    
-    # Compute correct answer
-    ans <- aggregate(value~lon+lat+time, data=d$val, FUN=weighted.mean, w=w)
-    
-    res1 <- makeZStat(d, verbose=F, sortData=F, FUN=weighted.mean, w)
-    expect_is(res1, "cmip5data")
-    
-    myfunc <- function(x, w, ...) weighted.mean(x, w, ...)
-    res2 <- makeZStat(d, verbose=F, sortData=F, FUN=myfunc, w)
-    expect_is(res1, "cmip5data")
-    
-    # Are the result values correct?    
-    expect_equal(res1$val$value, ans$value)
-    expect_equal(res2$val$value, ans$value)
+    for(i in implementations) {
+        d <- cmip5data(years, lonsize=llsize, latsize=llsize, Z=T, loadAs=i)
+        
+        # All data 1, except max Z is 2
+        if(is.data.frame(d$val)) {
+            d$val$value <- 1
+            d$val$value[d$val$Z ==max(d$Z)] <- 2                
+        } else if(is.array(d$val)) {
+            d$val <- array(1, dim=dim(d$val))
+            d$val[,,length(d$Z),] <- 2          
+        }
+        w <- c(rep(1, length(d$Z)-1), length(d$Z)-1)
+        
+        # Compute correct answer
+        ans <- aggregate(value~lon+lat+time, data=d$val, FUN=weighted.mean, w=w)
+        
+        res1 <- makeZStat(d, verbose=F, sortData=F, FUN=weighted.mean, w)
+        expect_is(res1, "cmip5data", info=i)
+        
+        myfunc <- function(x, w, ...) weighted.mean(x, w, ...)
+        res2 <- makeZStat(d, verbose=F, sortData=F, FUN=myfunc, w)
+        expect_is(res1, "cmip5data", info=i)
+        
+        # Are the result values correct?    
+        expect_equal(RCMIP5:::vals(res1), ans$value, info=i)
+        expect_equal(RCMIP5:::vals(res2), ans$value, info=i)
+    }
 })
