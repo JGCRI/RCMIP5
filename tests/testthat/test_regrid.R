c5fa114cb429862692cc602f7a68bb6158fc381d
ktoddbrown <ktoddbrown@gmail.com>
2015-05-22 11:36:46 -0700

    minor formatting for line breaks

--- tests/testthat/test_regrid.R
+++ tests/testthat/test_regrid.R
@@ -18,8 +18,10 @@ test_that("regrid handles bad input", {
 test_that('regrid returns expected values for simple case', {
     numOrgLon <- 3
     numOrgLat <- 2
-    orgLon <- matrix(seq(0, 360-360/numOrgLon, by=360/numOrgLon) + 360/numOrgLon/2, nrow=numOrgLon, ncol=numOrgLat)
-    orgLat <- matrix(seq(-90, 90-180/numOrgLat, by=180/numOrgLat) + 180/numOrgLat/2, nrow=numOrgLon, ncol=numOrgLat, byrow=TRUE)
+    orgLon <- matrix(seq(0, 360-360/numOrgLon, by=360/numOrgLon) + 360/numOrgLon/2, 
+                     nrow=numOrgLon, ncol=numOrgLat)
+    orgLat <- matrix(seq(-90, 90-180/numOrgLat, by=180/numOrgLat) + 180/numOrgLat/2, 
+                     nrow=numOrgLon, ncol=numOrgLat, byrow=TRUE)
     
     orgArea <- list(lon = orgLon, lat=orgLat, val= matrix(1, nrow=numOrgLon, ncol=numOrgLat))
     orgVar <- cmip5data(x=list(lon = orgLon, lat=orgLat, val=rep(1:numOrgLon, times=numOrgLat)))
@@ -27,8 +29,10 @@ test_that('regrid returns expected values for simple case', {
    
     numProjLon <- 2
     numProjLat <- 3
-    projLon <- matrix(seq(0, 360-360/numProjLon, by=360/numProjLon) + 360/numProjLon/2, nrow=numProjLon, ncol=numProjLat)
-    projLat <- matrix(seq(-90, 90-180/numProjLat, by=180/numProjLat) + 180/numProjLat/2, nrow=numProjLon, ncol=numProjLat, byrow=TRUE)
+    projLon <- matrix(seq(0, 360-360/numProjLon, by=360/numProjLon) + 360/numProjLon/2, 
+                      nrow=numProjLon, ncol=numProjLat)
+    projLat <- matrix(seq(-90, 90-180/numProjLat, by=180/numProjLat) + 180/numProjLat/2, 
+                      nrow=numProjLon, ncol=numProjLat, byrow=TRUE)
     projArea <- list(lon = projLon, lat=projLat, val= matrix(1, nrow=numProjLon, ncol=numProjLat))
     projArea$val <- projArea$val/sum(projArea$val, na.rm=TRUE)*sum(orgArea$val, na.rm=TRUE)
     transferMatrix <- getProjectionMatrix(orgArea = orgArea, projArea=projArea)
@@ -45,7 +49,9 @@ test_that('regrid returns expected values for simple case', {
 
 test_that('regrid test for data', {
     skip_on_cran()
-    path='../../sampledata'
+    path <- "../../sampledata"
+    if(!file.exists(path)) skip("Path doesn't exist")
+    
     numProjLon <- 360
     numProjLat <- 180
     projLon <- matrix(seq(0, 360-360/numProjLon, by=360/numProjLon) + 360/numProjLon/2, nrow=numProjLon, ncol=numProjLat)
