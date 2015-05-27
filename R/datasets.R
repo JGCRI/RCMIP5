b69aa2e633e2b3f0bb1ad314c78e44424eec1b7a
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2015-05-27 14:14:46 -0400

    Test data set
    Re issue #129

---
+++ R/datasets.R
@@ -0,0 +1,12 @@
+#' Global summaries of selected CMIP5 variables.
+#'
+#' Description.
+#'
+#' @format A data frame with XX rows and YY variables:
+#' \describe{
+#'   \item{field1}{describe}
+#'   \item{field2}{describe}
+#'   ...
+#' }
+#' @source \url{http://cmip-pcmdi.llnl.gov/cmip5/}
+"cmip5global"
