2836f73b2c4fe4910bb11b4353754258889f82e0
ktoddbrown <ktoddbrown@gmail.com>
2015-06-05 12:48:30 -0700

    fixed references to internal function

--- tests/testthat/test_addProvenance.R
+++ tests/testthat/test_addProvenance.R
@@ -15,7 +15,7 @@ test_that("addProvenance handles bad input", {
 
 test_that("addProvenance initializes", {
     for(i in implementations) {
-        d <- addProvenance(cmip5data(1, loadAs=i), msg="test")
+        d <- RCMIP5:::addProvenance(cmip5data(1, loadAs=i), msg="test")
         expect_is(d, "cmip5data", info=i)
         expect_is(d$provenance, "data.frame", info=i)
     }
@@ -25,7 +25,7 @@ test_that("addProvenance adds messages", {
     for(i in implementations) {
         d <- cmip5data(1, loadAs=i)
         expect_equal(nrow(d$provenance), 2, info=i)            # One line for software specs, one for message
-        d <- addProvenance(d, "test23")
+        d <- RCMIP5:::addProvenance(d, "test23")
         expect_equal(nrow(d$provenance), 3, info=i)           # One line for software specs, one for message
         expect_true(any(grepl("test23", d$provenance[3,])), info=i)   # 'test23' should appear in final line
     }
@@ -35,7 +35,7 @@ test_that("addProvenance merges provenances", {
     for(i in implementations) {
         d1 <- cmip5data(1, loadAs=i)
         d2 <- cmip5data(2, loadAs=i)
-        d3 <- addProvenance(d1, d2)
+        d3 <- RCMIP5:::addProvenance(d1, d2)
         
         expect_equal(nrow(d1$provenance)+nrow(d2$provenance), nrow(d3$provenance), info=i)
     }
