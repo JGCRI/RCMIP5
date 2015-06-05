d8df831a852a2c49326ec4034597804df570f5ae
ktoddbrown <ktoddbrown@gmail.com>
2015-06-05 12:44:41 -0700

    add array implementation

--- R/makeMonthlyStat.R
+++ R/makeMonthlyStat.R
@@ -36,48 +36,66 @@ makeMonthlyStat <- function(x, verbose=FALSE, sortData=FALSE, FUN=mean, ...) {
     
     # Main computation code
     timer <- system.time({ # time the main computation, below
-        
-        # Suppress stupid NOTEs from R CMD CHECK
-        lon <- lat <- Z <- time <- month <- value <- `.` <- NULL
-        
-        # Put data in consistent order BEFORE overwriting time
-        if(sortData) {
-            if(verbose) cat("Sorting data...\n")    
-            x$val <- group_by(x$val, lon, lat, Z, time) %>%
-                arrange()
-        }
-        
-        monthIndex <- floor((x$val$time %% 1) * 12 + 1)
-        x$val$month <- monthIndex  
-        
-        # Instead of "summarise(value=FUN(value, ...))", we use the do()
-        # call below, because the former doesn't work (as of dplyr 0.3.0.9000):
-        # the ellipses cause big problems. This solution thanks to Dennis
-        # Murphy on the manipulatr listesrv.
-        x$val <- group_by(x$val, lon, lat, Z, month) %>%
-            do(data.frame(value = FUN(.$value, ...))) %>%
-            ungroup()
-        x$val$time <- x$val$month
-        x$val$month <- NULL
-        
-        # dplyr doesn't (yet) have a 'drop=FALSE' option, and the summarise
-        # command above may have removed some lon/lat combinations
-        if(length(unique(x$val$lon)) < length(unique(as.numeric(x$lon))) |
-               length(unique(x$val$lat)) < length(unique(as.numeric(x$lat)))) {
-            if(verbose) cat("Replacing missing lon/lat combinations\n")
+        if(is.array(x$val)){
             
-            # Fix this by generating all lon/lat pairs and combining with answer
-            full_data <- tbl_df(data.frame(lon=x$lon, lat=x$lat))
-            x$val <- left_join(full_data, x$val, by=c("lon", "lat"))
-        }
+            monthNum <- floor((x$time-floor(x$time)) * 12) + 1
+            newDim <- dim(x$val)
+            newDim[4] <- length(unique(monthNum))
+            #aggFUN <- FUN
+            x$time <- sort(unique(monthNum))
+            x$numYears <- as.data.frame(table(monthNum))$Freq
+            x$val <- vapply(x$time, FUN=function(monthIndex, ...){
+                newDim <- dim(x$val)
+                temp <- x$val[,,,monthNum==monthIndex]
+                newDim[4] <- sum(monthNum == monthIndex)
+                dim(temp) <- newDim
+                return(apply(temp, c(1,2,3), FUN, ...))
+            }, FUN.VALUE=x$val[,,,1], ...)
+            dim(x$val) <- newDim
+        }else{
+            # Suppress stupid NOTEs from R CMD CHECK
+            lon <- lat <- Z <- time <- month <- value <- `.` <- NULL
+            
+            # Put data in consistent order BEFORE overwriting time
+            if(sortData) {
+                if(verbose) cat("Sorting data...\n")    
+                x$val <- group_by(x$val, lon, lat, Z, time) %>%
+                    arrange()
+            }
+            
+            monthIndex <- floor((x$val$time %% 1) * 12 + 1)
+            x$val$month <- monthIndex  
+            
+            # Instead of "summarise(value=FUN(value, ...))", we use the do()
+            # call below, because the former doesn't work (as of dplyr 0.3.0.9000):
+            # the ellipses cause big problems. This solution thanks to Dennis
+            # Murphy on the manipulatr listesrv.
+            x$val <- group_by(x$val, lon, lat, Z, month) %>%
+                do(data.frame(value = FUN(.$value, ...))) %>%
+                ungroup()
+            x$val$time <- x$val$month
+            x$val$month <- NULL
+            
+            # dplyr doesn't (yet) have a 'drop=FALSE' option, and the summarise
+            # command above may have removed some lon/lat combinations
+            if(length(unique(x$val$lon)) < length(unique(as.numeric(x$lon))) |
+                   length(unique(x$val$lat)) < length(unique(as.numeric(x$lat)))) {
+                if(verbose) cat("Replacing missing lon/lat combinations\n")
+                
+                # Fix this by generating all lon/lat pairs and combining with answer
+                full_data <- tbl_df(data.frame(lon=x$lon, lat=x$lat))
+                x$val <- left_join(full_data, x$val, by=c("lon", "lat"))
+            }
+            x$time <- 1:12
+            x$numYears <- as.data.frame(table(floor(monthIndex)))$Freq
+        }#if(array) else
     }) # system.time
     
     if(verbose) cat('\nTook', timer[3], 's\n')
     
     # Finish up
-    x$numYears <- as.data.frame(table(floor(monthIndex)))$Freq
     x$timeUnit <- "months (summarized)"
-    x$time <- 1:12
+    
     addProvenance(x, paste("Calculated", 
                            paste(deparse(substitute(FUN)), collapse="; "),
                            "for months", min(x$time), "-", max(x$time)))
--- tests/testthat/test_makeMonthlyStat.R
+++ tests/testthat/test_makeMonthlyStat.R
@@ -12,6 +12,8 @@ library(testthat)
 
 context("makeMonthlyStat")
 
+implementations <- c("data.frame", "array")
+
 test_that("makeMonthlyStat handles bad input", {
     expect_error(makeMonthlyStat(1))                         # non-list d
     expect_error(makeMonthlyStat(cmpi5data()))               # wrong size list d
@@ -23,63 +25,73 @@ test_that("makeMonthlyStat handles bad input", {
 
 test_that("makeMonthlyStat handles monthly data", {
     years <- 1850:1851
-    d <- cmip5data(years)
-    res <- makeMonthlyStat(d, verbose=F)
-    
-    # Is 'res' correct type and size?
-    expect_is(res,"cmip5data")
-    
-    # Did unchanging info get copied correctly?
-    expect_equal(res$lon, d$lon)
-    expect_equal(res$lat, d$lat)
-    expect_equal(res$valUnit, d$valUnit)
-    expect_equal(res$files, d$files)
-    
-    # numYears set and provenance updated?
-    expect_is(res$numYears, "integer")
-    expect_more_than(nrow(res$provenance), nrow(d$provenance))
-    
-    # Does time match what we expect?
-    expect_equal(res$time, 1:12)
-    
-    # Is the answer value data frame correctly sized?
-    expect_equal(nrow(res$val), length(d$lon) * 12) 
-    expect_equal(length(res$time), 12)
-    
-    # Are the answer values numerically correct?    
-    d$val$time <- floor((d$val$time %% 1) * 12 + 1)
-    dummyans <- aggregate(value~lon+lat+time, data=d, FUN=mean)
-    expect_equal(dummyans$value, res$val$value)
+    for(i in implementations){
+        d <- cmip5data(years, randomize=TRUE, loadAs=i)
+        res <- makeMonthlyStat(d, verbose=F)
+        
+        # Is 'res' correct type and size?
+        expect_is(res,"cmip5data")
+        
+        # Did unchanging info get copied correctly?
+        expect_equal(res$lon, d$lon, info=i)
+        expect_equal(res$lat, d$lat, info=i)
+        expect_equal(res$valUnit, d$valUnit, info=i)
+        expect_equal(res$files, d$files, info=i)
+        
+        # numYears set and provenance updated?
+        expect_is(res$numYears, "integer", info=i)
+        expect_more_than(nrow(res$provenance), nrow(d$provenance), info=i)
+        
+        # Does time match what we expect?
+        expect_equal(res$time, 1:12, info=i)
+        
+        # Is the answer value data frame correctly sized?
+        expect_equal(RCMIP5:::nvals(res), length(d$lon) * 12, info=i) 
+        expect_equal(length(res$time), 12, info=i)
+        
+        # Are the answer values numerically correct?
+        dummyans <- as.data.frame(d)
+        dummyans$time <- floor((dummyans$time %% 1) * 12 + 1)
+        dummyans <- aggregate(value~lon+lat+time, data=dummyans, FUN=mean)
+        dummyans <- merge(dummyans, as.data.frame(res), by=c('lon', 'lat', 'time'))
+        expect_equal(dummyans$value.x, dummyans$value.y, info=i)
+    }
 })
 
 test_that("makeMonthlyStat handles annual data", {
     years <- 1850:1851
-    d <- cmip5data(years, monthly=F)
-    expect_error(makeMonthlyStat(d, verbose=F))
+    for(i in implementations){
+        d <- cmip5data(years, monthly=F, loadAs=i)
+        expect_error(makeMonthlyStat(d, verbose=F), info=i)
+    }
 })
 
 test_that("makeMonthlyStat handles 4-dimensional data", {
     years <- 1850:1851
-    d <- cmip5data(years, Z=T)
-    res <- makeMonthlyStat(d, verbose=F)
-    
-    # Do years match what we expect?
-    expect_equal(res$time, 1:12)
-    
-    # Is the answer value data frame correctly sized?
-    expect_equal(nrow(res$val), length(d$lon) * length(d$Z) * 12) 
-    expect_equal(length(res$time), 12)
-    
-    # Are the answer values numerically correct?    
-    d$val$time <- floor((d$val$time %% 1) * 12 + 1)
-    dummyans <- aggregate(value~lon+lat+Z+time, data=d, FUN=mean)
-    expect_equal(dummyans$value, res$val$value)
+    for(i in implementations){
+        d <- cmip5data(years, Z=T, randomize=TRUE, loadAs=i)
+        res <- makeMonthlyStat(d, verbose=F)
+        
+        # Do years match what we expect?
+        expect_equal(res$time, 1:12, info=i)
+        
+        # Is the answer value data frame correctly sized?
+        expect_equal(RCMIP5:::nvals(res), length(d$lon) * length(d$Z) * 12, info=i) 
+        expect_equal(length(res$time), 12, info=i)
+        
+        # Are the answer values numerically correct?  
+        dummyans <- as.data.frame(d)
+        dummyans$time <- floor((dummyans$time %% 1) * 12 + 1)
+        dummyans <- aggregate(value~lon+lat+Z+time, data=dummyans, FUN=mean)
+        dummyans <- merge(dummyans, as.data.frame(res), by=c('lon', 'lat', 'Z', 'time'))
+        expect_equal(dummyans$value.x, dummyans$value.y, info=i)
+    }
 })
 
 test_that("makeMonthlyStat handles custom function and dots", {
     years <- 1850:1851
     llsize <- 2
-    d <- cmip5data(years, lonsize=llsize, latsize=llsize)
+    d <- cmip5data(years, lonsize=llsize, latsize=llsize, loadAs='data.frame')
     
     # All 1850 data 1, all 1851 data 2
     d$val$value <- 1
@@ -90,14 +102,20 @@ test_that("makeMonthlyStat handles custom function and dots", {
     d$val$month <- floor((d$val$time %% 1) * 12 + 1)
     ans <- aggregate(value~lon+lat+month, data=d$val, FUN=weighted.mean, w=w)
     
-    res1 <- makeMonthlyStat(d, verbose=F, sortData=F, FUN=weighted.mean, w)
-    expect_is(res1, "cmip5data")
-    
-    myfunc <- function(x, w, ...) weighted.mean(x, w, ...)
-    res2 <- makeMonthlyStat(d, verbose=F, sortData=F, FUN=myfunc, w)
-    expect_is(res1, "cmip5data")
-    
-    # Are the answer values numerically correct?    
-    expect_equal(res1$val$value, ans$value)    
-    expect_equal(res2$val$value, ans$value)
+    for(i in implementations){
+        if(i %in% 'array'){
+            d$val <- as.array(d, drop=FALSE)
+        }
+        
+        res1 <- makeMonthlyStat(d, verbose=F, sortData=F, FUN=weighted.mean, w)
+        expect_is(res1, "cmip5data", info=i)
+        
+        myfunc <- function(x, w, ...) weighted.mean(x, w, ...)
+        res2 <- makeMonthlyStat(d, verbose=F, sortData=F, FUN=myfunc, w)
+        expect_is(res1, "cmip5data", info=i)
+        
+        # Are the answer values numerically correct?    
+        expect_equal(RCMIP5:::vals(res1), ans$value)    
+        expect_equal(RCMIP5:::vals(res2), ans$value)
+    }
 })
